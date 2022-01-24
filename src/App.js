4be0a25b4e9b64227bb9bec8629072c26737de72
Po Yu Chen <pyshif@gmail.com>
2022-01-24 19:54:31 +0800

    增加 Test.js 用於測試

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Ax from './utils/Ax';
 
 function App() {
     return (
-        <Router>
-            <>
-                <Switch>
-                    <Route>
-                        <Subscrible></Subscrible>
-                    </Route>
-                    <Route>
-                        <Checkout></Checkout>
-                    </Route>
-                    <Route>
-                        <Register></Register>
-                    </Route>
-                    <Route>
-                        <Order></Order>
-                    </Route>
-                    <Route path="">
-                        <OrderDetail></OrderDetail>
-                    </Route>
-                    <Route path="/cart">
-                        <Cart></Cart>
-                    </Route>
-                    <Route path="/favorite">
-                        <Favorite></Favorite>
-                    </Route>
-                    <Route path="/member/detail">
-                        <ProductDetail></ProductDetail>
-                    </Route>
-                    <Route path="/member">
-                        <Product></Product>
-                    </Route>
-                    <Route path="/member">
-                        <Member></Member>
-                    </Route>
-                    <Route path="/">
-                        <Home></Home>
-                    </Route>
-                </Switch>
-            </>
-        </Router>
+        <div>
+            <Ax></Ax>
+            {/* <Router>
+                <>
+                    <Switch>
+                        <Route>
+                            <Subscrible></Subscrible>
+                        </Route>
+                        <Route>
+                            <Checkout></Checkout>
+                        </Route>
+                        <Route>
+                            <Register></Register>
+                        </Route>
+                        <Route>
+                            <Order></Order>
+                        </Route>
+                        <Route path="">
+                            <OrderDetail></OrderDetail>
+                        </Route>
+                        <Route path="/cart">
+                            <Cart></Cart>
+                        </Route>
+                        <Route path="/favorite">
+                            <Favorite></Favorite>
+                        </Route>
+                        <Route path="/member/detail">
+                            <ProductDetail></ProductDetail>
+                        </Route>
+                        <Route path="/member">
+                            <Product></Product>
+                        </Route>
+                        <Route path="/member">
+                            <Member></Member>
+                        </Route>
+                        <Route path="/">
+                            <Home></Home>
+                        </Route>
+                    </Switch>
+                </>
+            </Router> */}
+        </div>
     );
 }
 
---
+++ src/utils/Ax.js
@@ -0,0 +1,20 @@
+// import { n42, n32 } from './export.js';
+// import n from './export.js';
+
+import Axios from './export';
+
+// default 的名字可以改
+
+function Ax() {
+    Axios.get();
+    // n();
+    // n32();
+    // console.log(Axios);
+    // Axios.get();
+    // Axios.put();
+    // Axios.post();
+    // Axios.delete();
+    return <div></div>;
+}
+
+export default Ax;
