68f98921f80bd63031435dd6d300560139da92bd
Po Yu Chen <pyshif@gmail.com>
2022-01-24 19:54:31 +0800

    增加 Test.js 用於測試

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Exp from './utils/Exp';
 
 function App() {
+    Exp.get();
     return (
-        <Router>
-            <>
-                <Switch>
-                    <Route>
-                        <Subscrible></Subscrible>
-                    </Route>
-                    <Route>
-                        <Checkout></Checkout>
-                    </Route>
-                    <Route>
-                        <Register></Register>
-                    </Route>
-                    <Route>
-                        <Order></Order>
-                    </Route>
-                    <Route path="">
-                        <OrderDetail></OrderDetail>
-                    </Route>
-                    <Route path="/cart">
-                        <Cart></Cart>
-                    </Route>
-                    <Route path="/favorite">
-                        <Favorite></Favorite>
-                    </Route>
-                    <Route path="/member/detail">
-                        <ProductDetail></ProductDetail>
-                    </Route>
-                    <Route path="/member">
-                        <Product></Product>
-                    </Route>
-                    <Route path="/member">
-                        <Member></Member>
-                    </Route>
-                    <Route path="/">
-                        <Home></Home>
-                    </Route>
-                </Switch>
-            </>
-        </Router>
+        <div>
+            {/* <Router>
+                <>
+                    {/* Switch 只會返回一個元件 */}
+                    <Switch>
+                        {/* <Route path="/member">
+                            <Subscrible></Subscrible>
+                        </Route> */}
+                        <Route path="/member/:number?">
+                            <Subscrible></Subscrible>
+                        </Route>
+                        <Route>
+                            <Checkout></Checkout>
+                        </Route>
+                        <Route>
+                            <Register></Register>
+                        </Route>
+                        <Route>
+                            <Order></Order>
+                        </Route>
+                        <Route path="">
+                            <OrderDetail></OrderDetail>
+                        </Route>
+                        <Route path="/cart">
+                            <Cart></Cart>
+                        </Route>
+                        <Route path="/favorite">
+                            <Favorite></Favorite>
+                        </Route>
+                        <Route path="/member/detail">
+                            <ProductDetail></ProductDetail>
+                        </Route>
+                        <Route path="/member">
+                            <Product></Product>
+                        </Route>
+                        <Route path="/member">
+                            <Member></Member>
+                        </Route>
+                        <Route path="/">
+                            <Home></Home>
+                        </Route>
+                    </Switch>
+                </>
+            </Router> */}
+        </div>
     );
 }
 
---
+++ src/utils/Exp.js
@@ -0,0 +1,21 @@
+export default class {
+    constructor(url) {
+        this.url;
+    }
+
+    get() {
+        console.log('this is get method', name);
+    }
+
+    post() {
+        console.log('this is post method', name);
+    }
+
+    put() {
+        console.log('this is put method', name);
+    }
+
+    delete() {
+        console.log('this is delete method', name);
+    }
+}
