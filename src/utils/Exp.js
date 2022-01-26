8ad06505ab0aead7abff0e70bad1725f573190f9
Po Yu Chen <pyshif@gmail.com>
2022-01-26 20:42:07 +0800

    建立頁面、通用元件

--- src/utils/Exp.js
+++ src/utils/Exp.js
@@ -16,3 +16,26 @@ export default {
     user: 'liz',
     whoUser,
 };
+
+// export function foo1() {}
+
+// export function foo2() {}
+
+// export default () {}
+
+// import { foo1, foo2 } from 'Exp';
+
+// import FOO3 from 'Exp';
+
+// exports = module.exports = {}
+
+// exports.foo = function() {}
+
+// const foo2 = {};
+
+// module.exports = {foo1, foo2};
+// module.exports = myObject;
+
+// return module.exports = { foo1, foo2, };
+
+// const foo = require('123');
