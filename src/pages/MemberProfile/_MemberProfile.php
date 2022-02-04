de6e5175d8fd7da96558bcde3df076894c746f89
Po Yu Chen <pyshif@gmail.com>
2022-02-05 01:57:43 +0800

    重構 pages, components 檔案結構

---
+++ src/pages/MemberProfile/_MemberProfile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<?php include "../../components/Head/_Head.php" ?>
+
+<body>
+
+<!-- navbar -->
+<?php include "../../components/Navbar/_Navbar.php" ?>
+
+<!-- breadcrumb -->
+<?php include "../../components/Breadcrumb/_Breadcrumb.php" ?>
+
+<!-- member sidebar left -->
+<?php include "../../components/MemberSideBarLeft/_MemberSideBarLeft.php" ?>
+
+<!-- member main -->
+<?php include "../../components/MemberMain/_MemberMain.php" ?>
+
+<!-- member sidebar right -->
+<?php include "../../components/MemebrSideBarRight/_MemberSideBarRight.php" ?>
+
+<!-- footer -->
+<?php include "../../components/Footer/_Footer.php" ?>
+    
+</body>
+</html>
