89a084416ec9797d5a9b489825c0c7237c87810b
Po Yu Chen <pyshif@gmail.com>
2022-02-05 16:34:00 +0800

    重構 Navbar 草稿為 _Navbar.php

--- src/components/Navbar/_Navbar.php
+++ src/components/Navbar/_Navbar.php
@@ -1,58 +1,52 @@
 <header class="navbar">
-        <!-- container 只是一個有 RWD 和預設 margin, padding 的容器 -->
-        <div class="container-lg">
-            <!-- Menu Bar -->
-            <div class="position-absolute d-lg-none ms-2" id="menu-bar">
-                <a href=".multi-link" , data-bs-toggle="collapse" type="button" aria-expand="false"
-                    aria-controls="link-group">
-                    <i class="fas fa-bars"></i>
-                </a>
-            </div>
-            <!-- Logo -->
-            <div class="mx-auto m-lg-0 me-lg-auto" id="logo">
-                <img src="../../images/colorful-logo.png" alt="">
-            </div>
-            <!-- Link Group -->
-            <ul class="nav collapse text-center multi-link" id="link-group">
-                <li class="col-12 col-lg-auto col-lg-auto nav-item mx-lg-3">
-                    <a class="nav-link fs-6" id="link-product">Product</a>
-                </li>
-                <li class="col-12 col-lg-auto nav-item mx-lg-3">
-                    <a class="nav-link fs-6" id="link-tutorial">Tutorial</a>
-                </li>
-                <li class="col-12 col-lg-auto nav-item mx-lg-3">
-                    <a class="nav-link fs-6" id="link-about">About us</a>
-                </li>
-                <li class="col-12 col-lg-auto nav-item mx-lg-3">
-                    <a class="nav-link fs-6" id="link-support">Support</a>
-                </li>
-            </ul>
-            <!-- link icons -->
-            <ul class="nav flex-nowrap text-center collapse d-lg-flex multi-link" id="icon-group">
-                <li class="col nav-item mx-md-1 mx-lg-2">
-                    <a class="nav-icon fs-6" href="#" id="link-icon-envelope">
-                        <i class="fas fa-envelope"></i>
-                    </a>
-                </li>
-                <li class="col nav-item mx-md-1 mx-lg-2">
-                    <a class="nav-icon fs-6" href="#" id="link-icon-heart">
-                        <i class="fas fa-heart"></i>
-                    </a>
-                </li>
-                <li class="col nav-item mx-md-1 mx-lg-2">
-                    <a class="nav-icon fs-6" href="#" id="link-icon-user">
-                        <i class="fas fa-user"></i>
-                        <!-- 登入 -->
-                        <!-- <i class="fas fa-sign-in-alt"></i> -->
-                        <!-- 登出 -->
-                        <!-- <i class="fas fa-sign-out-alt"></i> -->
-                    </a>
-                </li>
-                <li class="col nav-item mx-md-1 mx-lg-2">
-                    <a class="nav-icon fs-6" href="#" id="link-icon-cart">
-                        <i class="fas fa-shopping-cart"></i>
-                    </a>
-                </li>
-            </ul>
+    <div class="container">
+        <!-- company brand -->
+        <figure class="m-0 me-md-auto company-brand">
+            <img src="../../images/colorful-logo.png" alt="">
+        </figure>
+        <!-- menu -->
+        <div class="menu">
+            <button class="btn" data-bs-toggle="collapse" data-bs-target=".nav.collapse" aria-expand="false" aria-controls=".nav">
+                <i class="fas fa-bars"></i>
+            </button>
         </div>
-    </header>
+        <!-- links -->
+        <ul class="col-12 col-md-auto nav text-center collapse d-md-flex links">
+            <li class="col-12 col-md-auto nav-item">
+                <a href="#" class="nav-link">Product</a>
+            </li>
+            <li class="col-12 col-md-auto nav-item">
+                <a href="#" class="nav-link">Tutorial</a>
+            </li>
+            <li class="col-12 col-md-auto nav-item">
+                <a href="#" class="nav-link">About us</a>
+            </li>
+            <li class="col-12 col-md-auto nav-item">
+                <a href="#" class="nav-link">Support</a>
+            </li>
+        </ul>
+        <!-- icons -->
+        <ul class="col-12 col-md-auto nav text-center collapse d-md-flex icons">
+            <li class="col col-md-auto nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </li>
+            <li class="col col-md-auto nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-heart"></i>
+                </a>
+            </li>
+            <li class="col col-md-auto nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-user"></i>
+                </a>
+            </li>
+            <li class="col col-md-auto nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-shopping-cart"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
