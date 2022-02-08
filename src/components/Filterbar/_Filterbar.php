4739b654c2aad7fff4a44628e66a4e7e069b812a
Queena Tao <queena113@gmail.com>
2022-02-08 20:57:13 +0800

    finsh 商品列表

--- src/components/Filterbar/_Filterbar.php
+++ src/components/Filterbar/_Filterbar.php
@@ -1,17 +1,23 @@
-<div class="filter">
-    <ul class="sort-series">
-        <li class="active">全部</li>
-        <li>食物</li>
-        <li>風景</li>
-        <li>婚禮</li>
-        <li>底片</li>
-        <li>人像</li>
-    </ul>
-    <ul class="sort-icon">
-        <li><img src="../../icons/Sortbar/Layout.png"></li>
-        <li><img src="../../icons/Sortbar/Rows.png"></li>
-        <li><img src="../../icons/Sortbar/Arrow up.png"></li>
-        <li><img src="../../icons/Sortbar/Arrow down.png"></li>
-    </ul>
-    <div class="line"></div>
+<div class="filter mt-sm-5 my-sm-3 my-md-5">
+    <div class="filter-box">
+        <ul class="sort-series p-0">
+            <li class="active py-1 px-2 py-md-2 px-md-3">全部</li>
+            <li class="py-1 px-2 py-md-2 px-md-3">食物</li>
+            <li class="py-1 px-2 py-md-2 px-md-3">風景</li>
+            <li class="py-1 px-2 py-md-2 px-md-3">婚禮</li>
+            <li class="py-1 px-2 py-md-2 px-md-3">底片</li>
+            <li class="py-1 px-2 py-md-2 px-md-3">人像</li>
+        </ul>
+
+        <ul class="sort-icon p-0">
+            <li><img src="../../icons/Sortbar/Layout.png"></li>
+            <li><img src="../../icons/Sortbar/Rows.png"></li>
+            <li><img src="../../icons/Sortbar/Arrow up.png"></li>
+            <li><img src="../../icons/Sortbar/Arrow down.png"></li>
+        </ul>
+
+    </div>
+    <div class="line">
+
+    </div>
 </div>
