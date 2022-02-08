34ec13ba815bc32786a47924b0f0e5397ecd1cb1
Queena Tao <queena113@gmail.com>
2022-02-08 16:46:42 +0800

    productlist 爆版

---
+++ src/components/Filterbar/_Filterbar.php
@@ -0,0 +1,17 @@
+<div class="filter">
+    <ul class="sort-series">
+        <li class="active">全部</li>
+        <li>食物</li>
+        <li>風景</li>
+        <li>婚禮</li>
+        <li>底片</li>
+        <li>人像</li>
+    </ul>
+    <ul class="sort-icon">
+        <li><img src="../../icons/Sortbar/Layout.png"></li>
+        <li><img src="../../icons/Sortbar/Rows.png"></li>
+        <li><img src="../../icons/Sortbar/Arrow up.png"></li>
+        <li><img src="../../icons/Sortbar/Arrow down.png"></li>
+    </ul>
+    <div class="line"></div>
+</div>
