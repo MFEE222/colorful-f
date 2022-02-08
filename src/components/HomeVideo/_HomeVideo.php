5c3b91197f2a97299ec8904dca34dc0b30df7be3
jim60702 <95072583+jim60702@users.noreply.github.com>
2022-02-08 17:47:45 +0800

    新增home video物件

---
+++ src/components/HomeVideo/_HomeVideo.php
@@ -0,0 +1,6 @@
+<div class="fullscreen-video no-ie" id="js-fullscreen-video">
+    <video width=100% height=100% controls id="video" preload="preload" autoplay="autoplay" muted="muted" playsinline="playsinline" webkit-playsinline="webkit-playsinline">
+        <source src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4" type="video/mp4" />
+    </video>
+
+</div>
---
+++ src/pages/Home/Home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<?php include "../../components/Head/_Head.php" ?>
+
+<body>
+    <!-- navbar -->
+    <?php include "../../components/Navbar/_Navbar.php" ?>
+
+    <!-- video -->
+    <?php include "../../components/HomeVideo/_HomeVideo.php" ?>
+
+    <!-- footer -->
+    <?php include "../../components/Footer/_Footer.php" ?>
+
+</body>
+
+</html>
