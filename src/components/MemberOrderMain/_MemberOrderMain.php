ae3e25b62cee4349b35a0d5f3eeeb0273abb9938
Po Yu Chen <pyshif@gmail.com>
2022-02-08 15:33:18 +0800

    將 訂單篩選列抽出

--- src/components/MemberOrderMain/_MemberOrderMain.php
+++ src/components/MemberOrderMain/_MemberOrderMain.php
@@ -1,14 +1,6 @@
 <main class="col-12 col-md-10 member-order-main">
-    <div class="member-order-filter-bar">
-        <ul class="nav">
-            <li class="nav-item"><button class="nav-link btn">全部</button></li>
-            <li class="nav-item"><button class="nav-link btn">未付款</button></li>
-            <li class="nav-item"><button class="nav-link btn">已付款</button></li>
-            <li class="nav-item"><button class="nav-link btn">完成</button></li>
-            <li class="nav-item"><button class="nav-link btn">不成立</button></li>
-        </ul>
-        <hr>
-    </div>
+
+    <?php include "../../components/MemberOrderFilterBar/_MemberOrderFilterBar.php" ?>
 
     <?php include "../../components/MemberOrderCard/_MemberOrderCard.php" ?>
     <?php include "../../components/MemberOrderCard/_MemberOrderCard.php" ?>
--- src/pages/MemberProfile/_MemberProfile.php
+++ src/pages/MemberProfile/_MemberProfile.php
@@ -13,7 +13,7 @@
     <?php include "../../components/Breadcrumb/_Breadcrumb.php" ?>
 
 
-    <div class="container content">
+    <div class="container-fluid container-md content">
         <div class="row m-0">
             <!-- member sidebar left -->
             <?php include "../../components/MemberSideBarLeft/_MemberSideBarLeft.php" ?>
