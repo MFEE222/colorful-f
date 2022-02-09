6f51e21a9e9a63976d3d957ec09c77872568b9bf
Po Yu Chen <pyshif@gmail.com>
2022-02-09 11:29:41 +0800

    [FIX] @use "bootstrap" 導致 body 背景色被吃掉

--- src/components/MemberOrderDetailMain/_MemberOrderDetailMain.php
+++ src/components/MemberOrderDetailMain/_MemberOrderDetailMain.php
@@ -1,3 +1,4 @@
 <div class="col-12 col-md-10 member-order-detail">
     <?php include "../../components/MemberOrderFilterBar/_MemberOrderFilterBar.php" ?>
+    <?php include "../../components/MemberOrderStepProgressBar/_MemberOrderStepProgressBar.php" ?>
 </div>
