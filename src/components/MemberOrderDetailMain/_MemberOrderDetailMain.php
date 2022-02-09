46ef4687e8dd3f26eb05ea46f35e1ecec3348cb9
Po Yu Chen <pyshif@gmail.com>
2022-02-09 19:44:44 +0800

    進度備份 Order Detail / Info

--- src/components/MemberOrderDetailMain/_MemberOrderDetailMain.php
+++ src/components/MemberOrderDetailMain/_MemberOrderDetailMain.php
@@ -1,4 +1,16 @@
 <div class="col-12 col-md-10 member-order-detail">
-    <?php include "../../components/MemberOrderFilterBar/_MemberOrderFilterBar.php" ?>
-    <?php include "../../components/MemberOrderStepProgressBar/_MemberOrderStepProgressBar.php" ?>
+
+    <div class="container order-detail-card">
+        <!-- filter bar -->
+        <?php include "../../components/MemberOrderFilterBar/_MemberOrderFilterBar.php" ?>
+        <!-- step progress bar -->
+        <?php include "../../components/MemberOrderStepProgressBar/_MemberOrderStepProgressBar.php" ?>
+        <!-- order card -->
+        <?php include "../../components/MemberOrderDetailCard/_MemberOrderDetailCard.php" ?>
+        <?php include "../../components/MemberOrderDetailCard/_MemberOrderDetailCard.php" ?>
+        <?php include "../../components/MemberOrderDetailCard/_MemberOrderDetailCard.php" ?>
+        <!-- order info-->
+
+    </div>
+
 </div>
