cc1440086384c78ac4a795bbfafd8c20d3fb15fd
chiang-song-lang <afga3780@gmail.com>
2022-02-09 19:29:53 +0800

    signin j06t/6

---
+++ src/components/SigninMain/_SigninMain.php
@@ -0,0 +1,42 @@
+<main class="signin-main">
+    <div class="container">
+        <div class="row justify-content-center justify-content-lg-start">
+            <div class="col-12 col-sm-auto ">
+                <div class="form-box">
+                    <form class="signin-form" action="">
+
+                        <h4 class="box-title">SIGNIN TO YOUR COLORFUL</h4>
+
+                        <div class="form-floating">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="Email address">
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="forgot">
+                            <a href="" class="l-link">forgot password ?</a>
+                        </div>
+                        <div class="l-icon">
+                            <a href="" class="l-icon-link">
+                                <i class="fab fa-facebook-f fa-fw"></i>
+                            </a>
+                            <a href="" class="l-icon-link">
+                                <i class="fab fa-google fa-fw"></i>
+                            </a>
+                        </div>
+                        <div class="form-btn">
+                            <button type="submit">Sign in</button>
+                        </div>
+                        <div>
+                            <p class="signup">Don't have account?
+                                <a href="" class="l-link">Sign up</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
