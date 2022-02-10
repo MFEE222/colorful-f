3c9b4f920df7d1bc212dd940c8dc5ba80aad3aa5
alice <a0975012712@gmail.com>
2022-02-10 19:09:25 +0800

    進度備份 Teach Page

---
+++ src/pages/Teach/_Teach.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+<?php include "../../components/Head/_Head.php" ?>
+
+<body>
+    <div class="container-fluid">
+        <!-- 第一頁 -->
+        <div class="row pageOne align-items-center">
+            <!-- 標題 -->
+            <div class="col-12 col-md-4 p-3">
+                <h1 class="text-center">COLORFUL</h1>
+                <br />
+                <p class="text-center">A Better Way to Organize Your Recipe</p>
+            </div>
+
+            <!-- 第一張圖 -->
+            <div class="col-12 col-md-4 text-center p-3">
+                <img class="test" src="./../../images/iphone p1.png" />
+            </div>
+
+            <!-- 第二張圖 -->
+            <div class="d-none d-md-block col-md-4 p-3">
+                <img class="test" src="./../../images/iphone p1.png" />
+            </div>
+        </div>
+
+        <!--第二頁-->
+        <div class="row align-items-center bg-dark">
+            <div class="col-12 col-md-6 p-3">
+                <h1 class="text-light">
+                    How to Install Lightroom Presets <br />
+                    for Desktop and Mobile?
+                </h1>
+                <br />
+                <div class="buttons"></div>
+                <button class="raise">PDF download</button>
+            </div>
+
+            <div class="col-12 col-md-6 p-3 text-center">
+                <img class="test" src="./../../images/iphone p2.jpg" />
+            </div>
+        </div>
+
+        <!--第三頁-->
+
+        <div class="row vh-100">
+            <section id="LP3">
+                <div class="row">
+                    <div id="header" style="background-color: #ffff00">
+                        <h1 style="margin-bottom: 0">Video teach</h1>
+                    </div>
+                    <div class="col-12 col-xl-6 vd-box">
+                        <div class="box bg-dark p-3">
+                            <div class="ratios m-3">
+                                <iframe style="width: 100%; height: 500px"
+                                    src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
+                                    allowfullscreen></iframe>
+                            </div>
+
+
+                            <h3 class="text-light">Photographer's retouching process</h3>
+                            <p class="text-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet
+                                natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor rem,
+                                fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+        </section>
+    </div>
+
+
+
+    <div class="row vh-100">
+        <section id="LP4">
+            <h1>測試用</h1>
+        </section>
+    </div>
+
+    </div>
+</body>
