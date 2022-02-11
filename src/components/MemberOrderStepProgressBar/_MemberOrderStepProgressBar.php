cabe04b722ded317677d0c6b3af2190f0dfdc65b
Po Yu Chen <pyshif@gmail.com>
2022-02-11 15:14:48 +0800

    完成 訂單細節頁 OrderDetail Page

--- src/components/MemberOrderStepProgressBar/_MemberOrderStepProgressBar.php
+++ src/components/MemberOrderStepProgressBar/_MemberOrderStepProgressBar.php
@@ -1,4 +1,5 @@
-<div class="container member-step-progressbar">
+<!-- <div class="container member-step-progressbar"> -->
+<div class="member-step-progressbar">
     <ul class="row">
         <li class="col-3 active"><span class="text">訂單成立</span> <span class="d-block date">01/18 14:16</span></li>
         <li class="col-3"><span class="text">已付款</span> <span class="d-block date">01/19 09:22</span></li>
