27ed44e057f300b02899efe6248b32b883949ea5
Po Yu Chen <pyshif@gmail.com>
2022-02-11 11:59:49 +0800

    合併新圖

--- src/pages/Teach/_Teach.php
+++ src/pages/Teach/_Teach.php
@@ -3,25 +3,32 @@
 <?php include "../../components/Head/_Head.php" ?>
 
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid teach">
         <!-- 第一頁 -->
         <div class="row pageOne align-items-center">
             <!-- 標題 -->
-            <div class="col-12 col-md-4 p-3">
+            <div class="col-12 col-sm-4 p-3">
                 <h1 class="text-center">COLORFUL</h1>
                 <br />
                 <p class="text-center">A Better Way to Organize Your Recipe</p>
             </div>
 
-            <!-- 第一張圖 -->
-            
-            <div class="col-12 col-md-4 text-center p-3">
-                <img class="test" src="./../../images/iphone p1.png" />
-            </div>
-
-            <!-- 第二張圖 -->
-            <div class="d-none d-md-block col-md-4 p-3">
-                <img class="test" src="./../../images/iphone p1.png" />
+            <!-- 右側圖 -->
+            <div class="col-12 col-sm-8 row iphone">
+                <!-- iphone - 1 -->
+                <div class="col-12 col-sm-6 iphone-screen">
+                    <div class="iphone-inner">
+                        <img src="../../images/iphone-p1.png" alt="">
+                        <!-- <img src="./../../images/product-img.jpeg" alt=""> -->
+                    </div>
+                </div>
+                <!-- iphone - 2 -->
+                <div class="col-12 col-sm-6 iphone-screen">
+                    <!-- <img src="../../images/iphone-p1.png" alt=""> -->
+                    123
+                    <div class="iphone-inner">
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -38,7 +45,7 @@
             </div>
 
             <div class="col-12 col-md-6 p-3 text-center">
-                <img class="test" src="./../../images/iphone p2.jpg" />
+                <img class="test" src="./../../images/iphone-p2.jpg" />
             </div>
         </div>
 
@@ -65,39 +72,41 @@
                                 fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
                         </div>
                         <div id="header" style="background-color: #ffff00">
-                    </div>
-                    <div class="col-12 col-xl-6 vd-box">
-                        <div class="box bg-dark p-3">
-                            <div class="ratios m-3">
-                                <iframe style="width: 100%; height: 500px"
-                                    src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
-                                    allowfullscreen></iframe>
+                        </div>
+                        <div class="col-12 col-xl-6 vd-box">
+                            <div class="box bg-dark p-3">
+                                <div class="ratios m-3">
+                                    <iframe style="width: 100%; height: 500px"
+                                        src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
+                                        allowfullscreen></iframe>
+                                </div>
+
+                                <div>
+                                    <h3 class="text-light">Photographer's retouching process</h3>
+                                    <p class="text-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                        Eveniet
+                                        natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor
+                                        rem,
+                                        fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
+                                </div>
+
                             </div>
 
-<div>
-                            <h3 class="text-light">Photographer's retouching process</h3>
-                            <p class="text-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet
-                                natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor rem,
-                                fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
+
+
                         </div>
-
                     </div>
-
-                    
-
-                </div>
+            </section>
         </div>
-        </section>
-    </div>
 
 
 
-    <div class="row vh-100">
-        <section id="LP4">
-            <h1>測試用</h1>
-            <img class="test" src="./../../images/R.jpg" />
-        </section>
-    </div>
+        <div class="row vh-100">
+            <section id="LP4">
+                <h1>測試用</h1>
+                <img class="test" src="./../../images/R.jpg" />
+            </section>
+        </div>
 
     </div>
 </body>
