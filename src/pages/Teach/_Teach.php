d5fb967e36a9519df319932658f74e3bee287e9a
alice <a0975012712@gmail.com>
2022-02-11 17:59:40 +0800

    教學頁面初版

--- src/pages/Teach/_Teach.php
+++ src/pages/Teach/_Teach.php
@@ -18,19 +18,19 @@
                 <!-- iphone - 1 -->
                 <div class="col-12 col-sm-6 text-center iphone">
                     <div class="iphone-screen">
-                        <img src="../../images/iphone-0.png" alt="">
+                        <img src="../../images/iphone-p1.png" alt="">
                     </div>
                     <!-- 螢幕內容全部用這個包 -->
-                    <div class="screen-content">
+                    <!-- <div class="screen-content">
                         <div class="product">
                             <img src="../../images/product-img.jpeg" alt="">
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                 <!-- iphone - 2 -->
                 <div class="col-12 col-sm-6 text-center iphone">
                     <div class="iphone-screen">
-                        <img src="../../images/iphone-0.png" alt="">
+                        <img src="../../images/iphone-p1.png" alt="">
                     </div>
                     <!-- 螢幕內容全部用這個包 -->
                     <div class="screen-content">
@@ -41,71 +41,85 @@
         </div>
 
         <!--第二頁-->
-        <!-- <div class="row align-items-center bg-dark">
-            <div class="col-12 col-md-6 p-3">
-                <h1 class="text-light">
+        <div class="row page-two">
+
+            <div class="col-6 col-sm-6 text-light">
+                <h1 class="teach-font">How to Install Lightroom Presets <br />
+                    for Desktop and Mobile?</h1>
+
+                <div class="shape-ex3">
+                    <p>PDF download</p>
+                </div>
+            </div>
+
+
+            <!-- <h1 class="text-light">
                     How to Install Lightroom Presets <br />
                     for Desktop and Mobile?
                 </h1>
-                <br />
-                <div class="buttons"></div>
-                <button class="raise">PDF download</button>
-            </div>
+                <div class="shape-ex3">
+                    <p>PDF download</p>
+                </div> -->
 
-            <div class="col-12 col-md-6 p-3 text-center">
-                <img class="test" src="./../../images/iphone-p2.jpg" />
-            </div>
-        </div> -->
+            <!--第三頁-->
+            <div class="row page-three">
+                <h1 style="margin-bottom: 0">Video teach</h1>
 
-        <!--第三頁-->
-
-        <!-- <div class="row vh-100">
-            <section id="LP3">
-                <div class="row">
-                    <div id="header" style="background-color: #ffff00">
-                        <h1 style="margin-bottom: 0">Video teach</h1>
-                    </div>
-                    <div class="col-12 col-xl-6 vd-box">
-                        <div class="box bg-dark p-3">
-                            <div class="ratios m-3">
-                                <iframe style="width: 100%; height: 500px"
-                                    src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
-                                    allowfullscreen></iframe>
-                            </div>
-
-
-                            <h3 class="text-light">Photographer's retouching process</h3>
-                            <p class="text-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet
-                                natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor rem,
-                                fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
+                <!-- <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 ">
+                            <div class="card">
+                                <div class="card-image">
+                                    <div class="embed-responsive embed-responsive-16by9">
+                                        <iframe width="460" height="300" src="https://www.youtube.com/embed/SC1XE85BC9o"
+                                            frameborder="0" allowfullscreen></iframe>
+                                    </div> -->
+                <!--第一個視頻BOX-->
+                <div class="col-6  vd-box ">
+                    <div class="box bg-light px-3 py-3">
+                        <div class="ratios ">
+                            <iframe style=" width: 100%" src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
+                                allowfullscreen></iframe>
                         </div>
-                        <div id="header" style="background-color: #ffff00">
-                        </div>
-                        <div class="col-12 col-xl-6 vd-box">
-                            <div class="box bg-dark p-3">
-                                <div class="ratios m-3">
-                                    <iframe style="width: 100%; height: 500px"
-                                        src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
-                                        allowfullscreen></iframe>
-                                </div>
-
-                                <div>
-                                    <h3 class="text-light">Photographer's retouching process</h3>
-                                    <p class="text-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                        Eveniet
-                                        natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor
-                                        rem,
-                                        fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</p>
-                                </div>
-
-                            </div>
-
-
-
+                        <div>
+                            <h1>Photographer's retouching process</h1>
+                            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Eveniet
+                                natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor
+                                rem,
+                                fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</h4>
                         </div>
                     </div>
-            </section>
-        </div> -->
+
+                </div>
+                <!--第二個視頻BOX-->
+                <div class="col-6  vd-box ">
+                    <div class="box bg-light px-3 py-3">
+                        <div class="ratios ">
+                            <iframe style=" width: 100%" src="https://www.youtube.com/embed/-EKH1f1DdC8" frameborder="0"
+                                allowfullscreen></iframe>
+                        </div>
+                        <div>
+                            <h1>Photographer's retouching process</h1>
+                            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Eveniet
+                                natus expedita laboriosam ratione atque sapiente porro numquam enim neque dolor
+                                rem,
+                                fuga eos, maxime velit quibusdam veritatis recusandae harum voluptatum.</h4>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <!--第四頁-->
+                <div class="page-four">
+                    <h1>測試用</h1>
+                    <img class="4image" src="./../../images/R.jpg" />
+
+                </div>
+            </div>
+        </div>
 
 
 
