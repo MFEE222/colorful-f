0115a0aaf0ff99feaebee675abe42a9f6fbbceaa
chiang-song-lang <afga3780@gmail.com>
2022-02-14 00:39:58 +0800

    404 page 完成

---
+++ src/components/404/_404Main.php
@@ -0,0 +1,16 @@
+<main class="er-main">
+    <div class="container">
+        <div class="row er-text">
+            <div class="col-12">
+                <h2 class="er-title">404</h2>
+                <p class="er-subtitle">你要找的頁面迷路了</p>
+                <P class="er-subtitle-en">The page you are looking for lost</P>
+            </div>
+        </div>
+        <div class="row er-btn">
+            <div class="col-12">
+                <a class="er-button" href="">返回首頁</a>
+            </div>
+        </div>
+    </div>
+</main>
