f6eb61a50595def8144a910047e3f79f4285d9be
chiang-song-lang <afga3780@gmail.com>
2022-02-13 11:27:19 +0800

    team page

--- src/components/Team/_TeamMain.php
+++ src/components/Team/_TeamMain.php
@@ -1,84 +1,191 @@
 <main class="t-main">
-    <div class="t-head">
-        <h2 class="t-title">OUR TEAM</h2>
-        <p class="t-subtitle">Position/Role</p>
-    </div>
-    <div class="t-aboutus">
-        <img class="t-pic-l" src="../../images/履歷照.jpg" alt="">
-        <div class="t-text">
-            <h2 class="t-name">TAYLOR</h2>
-            <p class="t-project">Position/Role
-                This is your Team Member description. Use this space to write
-                a brief description of this person's role and responsibilities, or
-                add a short bio with a background summary. It's also a great
-                opportunity to highlight how this person is an asset to the team.
-            </p>
-        </div>
-        <img class="t-pic-r" src="../../images/履歷照.jpg" alt="">
-        <div class="t-text">
-            <h2 class="t-name">TAYLOR</h2>
-            <p class="t-project">Position/Role
-                This is your Team Member description. Use this space to write
-                a brief description of this person's role and responsibilities, or
-                add a short bio with a background summary. It's also a great
-                opportunity to highlight how this person is an asset to the team.
-            </p>
-        </div>
-        <img class="t-pic-l" src="../../images/履歷照.jpg" alt="">
-        <div class="t-text">
-            <h2 class="t-name">TAYLOR</h2>
-            <p class="t-project">Position/Role
-                This is your Team Member description. Use this space to write
-                a brief description of this person's role and responsibilities, or
-                add a short bio with a background summary. It's also a great
-                opportunity to highlight how this person is an asset to the team.
-            </p>
-        </div>
-        <img class="t-pic-r" src="../../images/履歷照.jpg" alt="">
-        <div class="t-text">
-            <h2 class="t-name">TAYLOR</h2>
-            <p class="t-project">Position/Role
-                This is your Team Member description. Use this space to write
-                a brief description of this person's role and responsibilities, or
-                add a short bio with a background summary. It's also a great
-                opportunity to highlight how this person is an asset to the team.
-            </p>
-        </div>
-        <img class="t-pic-l" src="../../images/履歷照.jpg" alt="">
-        <div class="t-text">
-            <h2 class="t-name">TAYLOR</h2>
-            <p class="t-project">Position/Role
-                This is your Team Member description. Use this space to write
-                a brief description of this person's role and responsibilities, or
-                add a short bio with a background summary. It's also a great
-                opportunity to highlight how this person is an asset to the team.
-            </p>
-        </div>
-    </div>
-    <div class="t-skill">
-        <div class="t-left">
-            <h2>Skill Tree</h2>
-            <button>button</button>
-        </div>
-        <div class="t-right">
-
-        </div>
-    </div>
-
-    <div class="t-contact">
-        <h2>Contact us</h2>
-        <map name=""></map>
-        <form action="">
-            <div class="t-floating">
-                <input type="" class="form-control" id="" placeholder="">
-                <label for=""></label>
+    <div class="container">
+        <div class="page-one">
+            <div class="t-head">
+                <h2 class="t-title">OUR TEAM</h2>
+                <p class="t-subtitle">COLORFUL</p>
             </div>
-            <div class="t-floating">
-                <input type="" class="form-control" id="" placeholder="">
-                <label for=""></label>
+            <!-- 圖文 + 文字方塊 -->
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <p class="d-block text-p">
+                        切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                    </p>
+                </div>
             </div>
-            
-            <button type="submit">button</button>
-        </form>
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0 pic-right">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <p class="d-block text-p">
+                        切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                    </p>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <p class="d-block text-p">
+                        切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                    </p>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <p class="d-block text-p">
+                        切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                    </p>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">陳博榆</h2>
+                    <p class="d-block text-p">切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                </div>
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">江致緯</h2>
+                    <p class="d-block text-p">
+                        切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">梁詩葶</h2>
+                    <p class="d-block text-p">切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                </div>
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row t-aboutus">
+                <!-- 圖片 -->
+                <div class="col-12 col-lg-6 p-0">
+                    <div class="ratio ratio-16x9">
+                        <img src="../../images/team.jpg" alt="">
+                    </div>
+                </div>
+
+                <!-- 文字 -->
+                <div class="col-12 col-lg-6 t-text">
+                    <h2 class="d-block text-n">蔣頌蓮</h2>
+                    <p class="d-block text-p">切版、RWD、動態效果<br>
+                        商品列表 商品內容<br>
+                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
+                        網站UI/UX設計與規劃<br>
+                        資料蒐集、文案撰寫 企劃書撰寫</p>
+                </div>
+            </div>
+        </div>
+        <div class="row t-skill">
+            <div class="col-12 col-lg-3 t-left">
+                <h2 class="t-k-title">Skill Tree</h2>
+                <p>
+                    React<br>
+                    Javascript<br>
+                    PHP<br>
+                    Node.js<br>
+                </p>
+            </div>
+            <div class="col-12 col-lg-9 t-right">
+                <img class="t-circle" src="../../images/圓餅圖.png" alt="">
+            </div>
+        </div>
+
+        <div class="row t-contact">
+            <h2 class="t-c-title">Contact us</h2>
+            <div class="col-12 col-lg-6 t-c-left">
+                <img class="t-map" src="../../images/map.png" alt="">
+            </div>
+            <div class="col-12 col-lg-6 t-c-right">
+                <form action="">
+                    <div class="t-floating">
+                        <input type="email" class="form-control" id="email" placeholder="email">
+                        <label for="email"></label>
+                    </div>
+                    <div class="t-floating">
+                        <input type="" class="form-control" id="" placeholder="">
+                        <label for=""></label>
+                    </div>
+                    <button class="t-c-btn" type=" submit">SUBMIT</button>
+                </form>
+            </div>
+        </div>
     </div>
 </main>
