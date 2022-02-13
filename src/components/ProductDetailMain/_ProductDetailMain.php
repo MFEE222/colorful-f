fc44a6060f1b03a1cba8e3723775a7ab0dec82be
Queena Tao <queena113@gmail.com>
2022-02-14 03:10:17 +0800

    商品細節卡 帶細修button位置

---
+++ src/components/ProductDetailMain/_ProductDetailMain.php
@@ -0,0 +1,70 @@
+<!-- Product desciption -->
+<div class="detail-box row gx-5">
+    <div class="img-box col-md-6 ">
+        <div class="img-big">
+            <div class="ratios ">
+                <img src="../../images/navbar-ex-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="img-list d-flex mt-4">
+            <div class="col-3">
+                <div class="img-small">
+                    <div class="ratios ">
+                        <img src="../../images/navbar-ex-1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="img-small">
+                    <div class="ratios ">
+                        <img src="../../images/navbar-ex-1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="img-small">
+                    <div class="ratios ">
+                        <img src="../../images/navbar-ex-1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="img-small">
+                    <div class="ratios ">
+                        <img src="../../images/navbar-ex-1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-box col-md-6 d-flex flex-column ">
+        <div class="pb-lg-3">
+            <p class="product-name">Weeding 01</p>
+            <ul class="d-flex align-items-center pb-2 pb-md-1 pb-xl-4">
+                <li class="ps-0"><i class="fas fa-star"></i></li>
+                <li><i class="fas fa-star"></i></li>
+                <li><i class="fas fa-star"></i></li>
+                <li><i class="fas fa-star"></i></li>
+                <li><i class="fas fa-star"></i></li>
+                <p class="reviews-text ps-2">20/ Reviews</p>
+            </ul>
+            <p class="detail-text mb-md-2">
+                因其細緻自然的色調而成為人像、時尚和婚禮攝影師的最愛。低對比度柔和的色調可以讓您營造出美麗而寧靜的美感。這款底片可以強調所有色調，最能帶出過飽和色澤的底片種類，尤其暖色系顏色更是能栩栩如生因此比起拍攝人物，更適用於風景攝影。
+            </p>
+        </div>
+        <div class="d-flex justify-content
+        -between d-xl-block">
+            <div>
+                <div class="wish-list">
+                    <i class="fas fa-heart"></i>
+                </div>
+                <p class="price">NT 20</p>
+            </div>
+            <div>
+                <button class="add-cart">加入購物車</button>
+            </div>
+
+        </div>
+    </div>
+
+</div>
---
+++ src/pages/ProductDetail/_ProductDetail.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<?php include "../../components/Head/_Head.php" ?>
+
+<body>
+
+    <!-- navbar -->
+    <?php include "../../components/Navbar/_Navbar.php" ?>
+
+    <!-- breadcrumb -->
+    <?php include "../../components/Breadcrumb/_Breadcrumb.php" ?>
+
+
+    <div class="container content">
+        <!-- <div class="row"> -->
+        <?php include "../../components/ProductDetailMain/_ProductDetailMain.php" ?>
+        <!-- </div> -->
+    </div>
+
+
+    <!-- footer -->
+    <?php include "../../components/Footer/_Footer.php" ?>
+
+
+</body>
+
+</html>
