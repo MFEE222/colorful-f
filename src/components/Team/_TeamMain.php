0eeddc3e41822986d97759e8fe0adcbe7dde75fb
chiang-song-lang <afga3780@gmail.com>
2022-02-13 14:52:22 +0800

    team rwd j06t/6

--- src/components/Team/_TeamMain.php
+++ src/components/Team/_TeamMain.php
@@ -26,7 +26,7 @@
                 </div>
             </div>
             <!-- 2. 圖文 + 文字方塊 -->
-            <div class="row t-aboutus">
+            <div class="row flex-row-reverse t-aboutus">
                 <!-- 圖片 -->
                 <div class="col-12 col-lg-6 p-0 pic-right">
                     <div class="ratio ratio-16x9">
@@ -35,7 +35,7 @@
                 </div>
                 <!-- 文字 -->
                 <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <h2 class="d-block text-n">陳博榆</h2>
                     <p class="d-block text-p">
                         切版、RWD、動態效果<br>
                         商品列表 商品內容<br>
@@ -55,7 +55,7 @@
                 </div>
                 <!-- 文字 -->
                 <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <h2 class="d-block text-n">江致緯</h2>
                     <p class="d-block text-p">
                         切版、RWD、動態效果<br>
                         商品列表 商品內容<br>
@@ -66,7 +66,7 @@
                 </div>
             </div>
             <!-- 4. 圖文 + 文字方塊 -->
-            <div class="row t-aboutus">
+            <div class="row flex-row-reverse t-aboutus">
                 <!-- 圖片 -->
                 <div class="col-12 col-lg-6 p-0">
                     <div class="ratio ratio-16x9">
@@ -75,7 +75,7 @@
                 </div>
                 <!-- 文字 -->
                 <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">陶靖宇</h2>
+                    <h2 class="d-block text-n">梁詩葶</h2>
                     <p class="d-block text-p">
                         切版、RWD、動態效果<br>
                         商品列表 商品內容<br>
@@ -87,23 +87,6 @@
             </div>
             <!-- 5. 圖文 + 文字方塊 -->
             <div class="row t-aboutus">
-                <!-- 文字 -->
-                <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">陳博榆</h2>
-                    <p class="d-block text-p">切版、RWD、動態效果<br>
-                        商品列表 商品內容<br>
-                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
-                        網站UI/UX設計與規劃<br>
-                        資料蒐集、文案撰寫 企劃書撰寫</p>
-                </div>
-                <!-- 圖片 -->
-                <div class="col-12 col-lg-6 p-0">
-                    <div class="ratio ratio-16x9">
-                        <img src="../../images/team.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="row t-aboutus">
                 <!-- 圖片 -->
                 <div class="col-12 col-lg-6 p-0">
                     <div class="ratio ratio-16x9">
@@ -112,7 +95,7 @@
                 </div>
                 <!-- 文字 -->
                 <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">江致緯</h2>
+                    <h2 class="d-block text-n">蔣頌蓮</h2>
                     <p class="d-block text-p">
                         切版、RWD、動態效果<br>
                         商品列表 商品內容<br>
@@ -121,75 +104,39 @@
                         資料蒐集、文案撰寫 企劃書撰寫</p>
                 </div>
             </div>
-            <div class="row t-aboutus">
-                <!-- 文字 -->
-                <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">梁詩葶</h2>
-                    <p class="d-block text-p">切版、RWD、動態效果<br>
-                        商品列表 商品內容<br>
-                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
-                        網站UI/UX設計與規劃<br>
-                        資料蒐集、文案撰寫 企劃書撰寫</p>
+            <div class="row t-skill">
+                <div class="col-12 col-lg-3 t-left">
+                    <h2 class="t-k-title">Skill Tree</h2>
+                    <p>
+                        React<br>
+                        Javascript<br>
+                        PHP<br>
+                        Node.js<br>
+                    </p>
                 </div>
-                <!-- 圖片 -->
-                <div class="col-12 col-lg-6 p-0">
-                    <div class="ratio ratio-16x9">
-                        <img src="../../images/team.jpg" alt="">
-                    </div>
+                <div class="col-12 col-lg-9 t-right">
+                    <img class="t-circle" src="../../images/圓餅圖.png" alt="">
                 </div>
             </div>
-            <div class="row t-aboutus">
-                <!-- 圖片 -->
-                <div class="col-12 col-lg-6 p-0">
-                    <div class="ratio ratio-16x9">
-                        <img src="../../images/team.jpg" alt="">
-                    </div>
-                </div>
 
-                <!-- 文字 -->
-                <div class="col-12 col-lg-6 t-text">
-                    <h2 class="d-block text-n">蔣頌蓮</h2>
-                    <p class="d-block text-p">切版、RWD、動態效果<br>
-                        商品列表 商品內容<br>
-                        Functional map、Site map建置<br> MYSQL資料庫建立<br>
-                        網站UI/UX設計與規劃<br>
-                        資料蒐集、文案撰寫 企劃書撰寫</p>
+            <div class="row t-contact">
+                <h2 class="t-c-title">Contact us</h2>
+                <div class="col-12 col-lg-6 t-c-left">
+                    <img class="t-map" src="../../images/map.png" alt="">
+                </div>
+                <div class="col-12 col-lg-6 t-c-right">
+                    <form action="">
+                        <div class="t-floating">
+                            <input type="email" class="form-control" id="email" placeholder="email">
+                            <label for="email"></label>
+                        </div>
+                        <div class="t-floating">
+                            <input type="text" class="form-control" id="text" placeholder="text">
+                            <label for="text"></label>
+                        </div>
+                        <button class="t-c-btn" type=" submit">SUBMIT</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <div class="row t-skill">
-            <div class="col-12 col-lg-3 t-left">
-                <h2 class="t-k-title">Skill Tree</h2>
-                <p>
-                    React<br>
-                    Javascript<br>
-                    PHP<br>
-                    Node.js<br>
-                </p>
-            </div>
-            <div class="col-12 col-lg-9 t-right">
-                <img class="t-circle" src="../../images/圓餅圖.png" alt="">
-            </div>
-        </div>
-
-        <div class="row t-contact">
-            <h2 class="t-c-title">Contact us</h2>
-            <div class="col-12 col-lg-6 t-c-left">
-                <img class="t-map" src="../../images/map.png" alt="">
-            </div>
-            <div class="col-12 col-lg-6 t-c-right">
-                <form action="">
-                    <div class="t-floating">
-                        <input type="email" class="form-control" id="email" placeholder="email">
-                        <label for="email"></label>
-                    </div>
-                    <div class="t-floating">
-                        <input type="" class="form-control" id="" placeholder="">
-                        <label for=""></label>
-                    </div>
-                    <button class="t-c-btn" type=" submit">SUBMIT</button>
-                </form>
-            </div>
-        </div>
-    </div>
 </main>
