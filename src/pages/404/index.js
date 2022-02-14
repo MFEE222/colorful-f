c4e979dd67c8f2ec5445804c421500813a7d5ca6
chiang-song-lang <afga3780@gmail.com>
2022-02-14 23:10:57 +0800

    error 轉 react

---
+++ src/pages/404/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Navbar from '../../utils/Navbar';
+// import Footer from '../../utils/Footer';
+import ErrorContent from '../../components/404/ErrorContent';
+
+function Error(props) {
+    return (
+        <>
+            {/* <Navbar /> */}
+            
+            <ErrorContent />
+
+            {/* <Footer /> */}
+        </>
+    );
+}
+
+export default Error;
