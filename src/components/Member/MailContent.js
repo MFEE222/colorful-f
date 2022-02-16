fc816ff702f3bbcdc556a5c722b94b8b138b30d1
chiang-song-lang <afga3780@gmail.com>
2022-02-16 13:52:26 +0800

    mail page rwd j06t/6

--- src/components/Member/MailContent.js
+++ src/components/Member/MailContent.js
@@ -3,7 +3,57 @@ import React from 'react';
 function MailContent(props) {
     return (
         <div className="col-12 col-md-10 member-mail">
-            <div className="box">This is testing...</div>
+            <div className="container">
+                {/* title */}
+                <div className="row">
+                    <div className="col-auto">
+                        <h2 class="mail-title">信件總覽</h2>
+                    </div>
+                </div>
+                {/* card */}
+                <div className="row mail-card">
+                    <div className="col-auto">
+                        <h4>訂單通知</h4>
+                        <p>
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                        </p>
+                        <p>2022/01/18</p>
+                        <button class="btn ms-auto d-block">
+                            <span>查看信件</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="row mail-card">
+                    <div className="col-auto">
+                        <h4>訂單通知</h4>
+                        <p>
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                        </p>
+                        <p>2022/01/18</p>
+                        <button class="btn ms-auto d-block">
+                            <span>查看信件</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="row mail-card">
+                    <div className="col-auto">
+                        <h4>訂單通知</h4>
+                        <p>
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                            註冊會員成功，電子信箱已驗證，可以開始立即享受購物樂趣!
+                        </p>
+                        <p>2022/01/18</p>
+                        <button class="btn ms-auto d-block">
+                            <span>查看信件</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
