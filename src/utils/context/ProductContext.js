3a1647be20699362d12113527bca644d95e3f2ac
Po Yu Chen <pyshif@gmail.com>
2022-02-21 03:43:33 +0800

    完成 ProductContext 雛型

--- src/utils/context/ProductContext.js
+++ src/utils/context/ProductContext.js
@@ -8,11 +8,38 @@ const ProductContext = React.createContext(
 // Provider
 export function ProductProvider(props) {
     // state, hook
+    const [products, setProducts] = useState({});
+    // const products = [
+    //     { id: 1, name: 'rosa' },
+    //     { id: 2, name: 'chris' },
+    // ];
 
     // shared
+    const { other } = props.other !== undefined ? props : { other: {} };
     const shared = {
+        other,
+        products,
+        setProducts,
         say: 'Hey, Good Night',
     };
+
+    // life cycle
+    useEffect(function () {
+        // axios
+        // set state
+        setProducts([{ id: 1, name: 'chris' }]);
+
+        return function () {
+            console.log('Here is WillUnMount');
+        };
+    }, []); // DidMount
+
+    useEffect(
+        function () {
+            console.log('Here is DidUpdate');
+        },
+        [products]
+    );
 
     // render
     return (
@@ -30,7 +57,19 @@ export function ProductConsumer(props) {
 
 // useContext
 export function useProductContext() {
-    return React.useContext();
+    return React.useContext(ProductContext);
 }
 
-export default ProductContext;
+// State Format
+// products = {
+//     id,
+//     name,
+//     price,
+// }
+
+// shared (此設計允許使用者加入共享資料，但若同名時會被預設值覆蓋)
+// let { shared } = props;
+// if (shared === undefined) {
+//     shared = {};
+// }
+// shared.say = 'Hey, Good Night';
