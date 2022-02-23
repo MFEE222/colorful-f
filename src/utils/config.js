e12d7df0351321050bb1a6a97daca19c826ea97f
jim60702 <95072583+jim60702@users.noreply.github.com>
2022-02-23 17:02:19 +0800

    create tmp

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,19 @@
+import { useImperativeHandle } from 'react/cjs/react.production.min';
+
 export const API_URL =
     (process.env.EXPRESS_API_URL || 'http://localhost:3006') + '/api';
 
 export const IMG_URL = process.env.EXPRESS_API_URL || 'http://localhost:3006';
+
+// req:
+// {
+//     userid,
+// }
+// res:
+// {
+//     id,
+//     created_at,
+//     product_id,
+//     user_id,
+// }
+export const API_GET_WISHLIST = API_URL + '/wishlist';
