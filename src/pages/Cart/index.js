50ee3160a07ee34b763e70fca32d370545eb6369
chiang-song-lang <afga3780@gmail.com>
2022-03-04 10:56:18 +0800

    購物車頁完成

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import CartCard from '../../components/Cart/CartCard';
 import CartContent from '../../components/Cart/CartContent';
+import CartPayment from '../../components/Cart/CartPayment';
+import CartPaymentCard from '../../components/Cart/CartPaymentCard';
+
+
 
 
 function Cart(props) {
-    return <CartContent />
+    return <CartPaymentCard />
 
 }
 
