a1b28c94b62935b865016463cab570319379d6ba
chiang-song-lang <afga3780@gmail.com>
2022-03-04 19:38:10 +0800

    1

--- src/components/Cart/CartPayment.js
+++ src/components/Cart/CartPayment.js
@@ -31,7 +31,7 @@ function CartPayment(props) {
                     </div>
                 </div>
                 {/* 商品列表 */}
-                <div className="row cart-body">
+                {/* <div className="row cart-body">
                     <div className="col-1"></div>
                     <div className="col-3  col-md-2">
                         <img src={Product} alt="" />
@@ -46,28 +46,63 @@ function CartPayment(props) {
                     <div className="col-1">
                         {/* <i className="fas fa-trash-alt icon"></i>
                         <i className="far fa-heart icon"></i> */}
-                    </div>
+                {/* </div>
+                </div>  */}
+                <div className="row  my-1 card-house">
+                    {display.map((v, i) => {
+                        const img = `${IMG_URL2}/${v.products_img}/a1.jpg`;
+                        // console.log('img :>> ', img);
+                        return (
+                            <div
+                                className="col-6 col-md-4 mt-2 px-3 mb-2"
+                                key={v.id}
+                            >
+                                <div className="card-border ">
+                                    <div className="card-img position-relative">
+                                        <div className="ratios">
+                                            <img
+                                                className="img-fluid"
+                                                src={img}
+                                                alt=""
+                                            />
+                                        </div>
+                                        {v.status == 2 ? (
+                                            <span className="position-absolute top-0  translate-middle p-2  border-light rounded-circle download-badge">
+                                                <span className="badge bg-secondary">
+                                                    已下載
+                                                </span>
+                                            </span>
+                                        ) : (
+                                            ''
+                                        )}
+                                    </div>
+                                    <div className="card-body text-start p-0 my-2 my-md-4">
+                                        <label
+                                            htmlFor={v.id}
+                                            className="d-flex justify-content-between align-items-center"
+                                        >
+                                            <input
+                                                className="form-check-input m-0"
+                                                type="checkbox"
+                                                value={v.id}
+                                                id={v.id}
+                                                checked={v.check}
+                                                onChange={function (e) {
+                                                    handleCheck(e, i);
+                                                }}
+                                            />
+                                            <p className="card-title fw-bold">
+                                                {v.name}
+                                            </p>
+                                        </label>
+                                        {/* </div> */}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-                {/* 商品列表 */}
-                <div className="row cart-body">
-                    <div className="col-1"></div>
-                    <div className="col-3  col-md-2">
-                        <img src={Product} alt="" />
-                    </div>
-                    <div className="col-5 col-md-6">
-                        <h2>米其林五星級</h2>
-                        <p>Duis aliquam convallis nunc.</p>
-                    </div>
-                    <div className="col-2">
-                        <h3>$99</h3>
-                    </div>
-                    <div className="col-1">
-                        {/* <i className="fas fa-trash-alt icon"></i>
-                        <i className="far fa-heart icon"></i> */}
-                    </div>
-                </div>
-
-                {/* 選擇付款方式 */}
+                ;{/* 選擇付款方式 */}
                 <div className="row cart-payment">
                     <div className="title">
                         <h2>付款方式</h2>
@@ -90,7 +125,6 @@ function CartPayment(props) {
                         <CartPaymentCard />
                     </div>
                 </div>
-
                 {/* 總共 */}
                 <div className="row cart-total justify-content-end">
                     <div className="col-4 price">
