93c01a53d710a392ecb8dd3ebd571f1eee30332f
chiang-song-lang <afga3780@gmail.com>
2022-03-04 09:11:15 +0800

    購物車 清單頁完成

--- src/components/Cart/CartContent.js
+++ src/components/Cart/CartContent.js
@@ -181,6 +181,7 @@ function CartContent(props) {
             </div>
 
             
+            {/* 引入login */}
             {/* <div className="scroll-down">
                 SCROLL DOWN
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
