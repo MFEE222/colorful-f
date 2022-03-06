7704c7714f0ad0e5147cee05b58e2dc65761f4ca
Art-liang <a0975012712@gmail.com>
2022-03-06 20:47:52 +0800

    訂單頁樣式調整

--- src/components/Member/OrderCard.js
+++ src/components/Member/OrderCard.js
@@ -1,45 +1,46 @@
 import React from 'react';
+import { Link, useRouteMatch, useParams } from 'react-router-dom';
 import ImgProduct from '../../images/product-img.jpeg';
 
 function OrderCard(props) {
+    const match = useRouteMatch();
+
     return (
-        <div className="container order-card">
+        <div className="container p-0">
             {/* <!-- header --> */}
-            <div className="row order-card-header">
-                <p className="col-auto order-card-time">2020/01/01 00:00</p>
-                <p className="col-auto order-card-number">
+
+            <div className="d-flex review-card flex-wrap">
+                <p className="col-12 col-lg-4 mb-3">2020/01/01 00:00</p>
+                <p className="col-12 col-lg-8 mb-3">
                     訂單編號 <span>2135276BRUYSBNYK</span>
                 </p>
-            </div>
-            {/* <!-- body --> */}
-            <div className="row order-card-body">
-                <div className="col-auto order-card-img">
-                    <div className="ratio ratio-4x3">
-                        <img src={ImgProduct} alt="" />
+
+                <div className="row">
+                    <div className="col-auto col-md-10 review-card-img me-4">
+                        <div className="ratios">
+                            <img src={ImgProduct} alt="" />
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-9 row align-items-center align-content-center">
+                        <h4 className="col-10  align-self-center m-0">
+                            Product
+                        </h4>
+                        <h4 className="col-2 order-card-price">$10</h4>
+                        <p className="col-10 me-auto order-card-descp content">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Vitae omnis, distinctio deleniti culpa
+                            voluptate odio perferendis quisquam ipsam, doloribus
+                            ab nulla hic asperiores? Quaerat repellendus
+                            suscipit voluptate eum itaque. Veniam.
+                        </p>
                     </div>
                 </div>
-                <div className="col row align-items-center align-content-center">
-                    <h5 className="col-6 me-auto order-card-title">Product</h5>
-                    <p className="col order-card-price">$10</p>
-                    <p className="col-10 col-md-6 me-auto order-card-descp">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Vitae omnis, distinctio deleniti culpa voluptate odio
-                        perferendis quisquam ipsam, doloribus ab nulla hic
-                        asperiores? Quaerat repellendus suscipit voluptate eum
-                        itaque. Veniam.
-                    </p>
-                    {/* <!-- <p className="col-auto order-card-status">Pending</p> --> */}
-                    {/* <!-- <div className="col align-self-end order-card-status">
-                <button className="btn ms-auto d-block">
-                    <span>Pending</span>
-                </button>
-            </div> --> */}
-                </div>
-                <hr />
+
                 <div className="col align-self-end order-card-status">
-                    <button className="btn ms-auto d-block">
-                        <span>Pending</span>
-                    </button>
+                    <div className="d-none d-md-block">
+                        <hr />
+                    </div>
+                    <button className="ms-auto d-block btn">Pending</button>
                 </div>
             </div>
         </div>
