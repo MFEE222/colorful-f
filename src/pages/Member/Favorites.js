b7add8ecbef16d8ab4a04ed5a2a500e7744c7cb8
jim60702 <95072583+jim60702@users.noreply.github.com>
2022-03-07 17:38:29 +0800

    新增 favoritesContext/抓favorites

--- src/pages/Member/Favorites.js
+++ src/pages/Member/Favorites.js
@@ -1,14 +1,14 @@
 //內建庫
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useLocation, Redirect } from 'react-router-dom';
+
 //第三方庫
 import axios from 'axios';
 // 共用
 import {
-    API_URL,
-    IMG_URL,
-    API_GET_WISHLIST,
+    API_GET_MEMBER_DOWNLOAD,
     IMG_URL2,
+    API_POST_MEMBER_DOWNLOAD_DNG,
 } from '../../utils/config';
 //Components
 // import FilterBarContent from '../../components/Filterbar/FilterBarContent';
@@ -23,172 +23,229 @@ import decBar from '../../icons/dec-bar.png';
 
 //元件
 import Pagination from '../../utils/Pagination';
+import Img1 from '../../images/婚禮_5.jpg';
 import Img2 from '../../images/wed001.jpg';
+import Auth from '../../pages/Auth';
 function Favorites(props) {
+    //TODO:1.連接資料庫拿下載資料; 2.用按鈕判斷要哪種 3.用狀態紀錄勾選哪些
+    // auth.current
+    let uid = 1;
+    const [checked, setChecked] = useState(false); //核取方塊
+    const [statusId, setStatusId] = useState(0); // filter 狀態
+    // const [download, setDownload] = useState([]); //給後端的{uid,pid....}要下載的東西
+    const [counts, setCounts] = useState(); //資料總計
+    const [offset, setOffset] = useState(1); //分頁
+    const [display, setDisplay] = useState([]); //呈現
+    console.log(123);
+    const fetchDownload = async () => {
+        const response = await axios.get(API_GET_MEMBER_DOWNLOAD, {
+            params: {
+                uid,
+                statusId, //all->0->status 1 & 2 //未下載status = 1 // 已下載 status = 2
+                offset,
+            },
+        });
+        // console.log('response.data.data :>> ', response.data);
+        const newDisplay = response.data.data.map(function (e) {
+            e.check = false;
+            return e;
+        });
+        console.log('newDisplay :>> ', newDisplay);
+        setCounts(response.data.rows);
+        // setDisplay(response.data.data);
+        setDisplay(newDisplay);
+    };
+
+    //checkbox
+    //all
+    const handleCheckAll = () => {
+        const c = !checked;
+        const newDisplay = [...display];
+
+        newDisplay.map((v, i) => {
+            v.check = c;
+            setDisplay(newDisplay);
+        });
+        setChecked(c);
+    };
+
+    //alone
+    const handleCheck = (event, index) => {
+        const newDisplay = [...display];
+        newDisplay[index].check = event.target.checked;
+        setDisplay(newDisplay);
+    };
+
+    const handleDownload = async () => {
+        const d = display.filter((v, i) => {
+            return v.check === true;
+        });
+        const dngId = d.map((v) => {
+            return v.product_id;
+        });
+        console.log('here :>> ', 'here');
+        const response = await axios.post(API_POST_MEMBER_DOWNLOAD_DNG, {
+            uid,
+            dngId,
+        });
+    };
     return (
         <div className="col-12 col-md-10 box">
             <div className="list-border">
-                <div className="product-list">
-                    {/* xs,sm 搜尋框 */}
-                    <div className="container d-md-flex d-lg-none justify-content-between my-md-3">
-                        <div className="phone-search d-flex justify-content-between ">
-                            <input
-                                type="search"
-                                className="input-style"
-                                placeholder="在此輸入關鍵字"
-                            />
-                            <div className="search-icon d-flex my-2">
-                                <div className="me-2">
-                                    <img
-                                        className="icon-img"
-                                        src={deleteImg}
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="me-2">
-                                    <img
-                                        className="icon-img"
-                                        src={searchImg}
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
+                <div className="member1-comment download">
+                    <div className="filter mt-sm-3 my-sm-3 my-md-3">
+                        <div className="filter-box d-flex">
+                            <ul className="sort-series p-0">
+                                <li
+                                    className="active py-2 px-3"
+                                    onClick={function () {
+                                        setStatusId(0);
+                                        setOffset(1);
+                                    }}
+                                >
+                                    全部
+                                </li>
+                                <li
+                                    className="py-2 px-3"
+                                    onClick={function () {
+                                        setStatusId(1);
+                                        setOffset(1);
+                                    }}
+                                >
+                                    未下載
+                                </li>
+                                <li
+                                    className="py-2 px-3"
+                                    onClick={function () {
+                                        setStatusId(2);
+                                        setOffset(1);
+                                    }}
+                                >
+                                    已下載
+                                </li>
+                            </ul>
                         </div>
-                        {/* <!-- xs,sm filterbar --> */}
-                        <div className="phone-filter d-flex justify-content-between my-2">
-                            <p className="my-auto mx-2">search result counts</p>
-                            <div className="d-flex justify-content-end align-items-center">
-                                <div className="dropdown-style my-auto me-2">
-                                    <select
-                                        name="filter-select"
-                                        id="filter-select"
-                                        className="filter-select"
-                                    >
-                                        <option value="defalut">篩選</option>
-                                        <option value="all">全部</option>
-                                        <option value="member">會員</option>
-                                        <option value="food">食物</option>
-                                        <option value="scenery">風景</option>
-                                        <option value="wedding">婚禮</option>
-                                        <option value="portrait">人像</option>
-                                        <option value="negative">底片</option>
-                                    </select>
-                                </div>
-                                <div className="icon-g d-flex">
-                                    <div className="me-2">
-                                        <img
-                                            className="icon-img"
-                                            src={listViewImg}
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div>
-                                        <img
-                                            className="icon-img"
-                                            src={sortImg}
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="line"></div>
+                    </div>
+                    <div className="d-flex justify-content-center justify-content-md-end my-4">
+                        <div
+                            className="text-center py-2 download-btn me-3"
+                            onClick={handleCheckAll}
+                        >
+                            勾選全部
+                        </div>
+                        <div
+                            className="text-center py-2 px-3 download-btn"
+                            onClick={handleDownload}
+                        >
+                            取消收藏
                         </div>
                     </div>
-                </div>
-                {/* <!-- md 以上filterbar --> */}
-                <div className="container">
-                    <div className="d-none d-lg-block">
-                        {/* <FilterBarContent /> */}
-                    </div>
-                </div>
-                {/* <!-- card --> */}
-                {/* <div className="container wishlist-group">
-                    <div className=" card-group row my-4 mt-md-5 my-2">
-                        <div className="col-6 col-md-3">
-                            <div className="card-border " data-tilt>
-                                
-                                <div className="card-img ">
-                                    <div className="ratios">
-                                        <img src={cardDemo} alt="" />
-                                    </div>
-                                </div>
-                                <div className="card-body text-start p-0 my-2 my-md-4">
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <p className="card-title">
-                                            婚禮／Wedding-1
-                                        </p>
-                                    
-                                        <i className="fas fa-heart"></i>
-                                    </div>
-                                    <div className="text-start">
-                                        NT 20
-                                        <button className="btn btn-warning ">
-                                            <span>加入購物車</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
+                    {/* <!-- card --> */}
 
-                {/* <div className="col-6 col-md-4 mt-2 px-3 mb-2">
-                    <div className="card-border ">
-                        <div className="card-img position-relative">
-                            <div className="ratios">
-                                <img className="img-fluid" src="" alt="" />
-                            </div>
-                        </div>
-                        <div className="card-body text-start p-0 my-2 my-md-4">
-                            <label className="d-flex justify-content-between align-items-center">
-                                <p className="card-title fw-bold"></p>
-                            </label>
-                            
-                        </div>
-                    </div>
-                </div> */}
+                    <div className="row  my-1 card-house">
+                        {/* card樣式 */}
+                        {/* <div>
+                            <div className=" card-group row my-4 mt-md-5 my-2">
+                                <div className="col-6 col-md-3">
+                                    <div className="card-border " data-tilt>
+                                        <div className="card-img ">
+                                            <div className="ratios">
+                                                <img src={cardDemo} alt="" />
+                                            </div>
+                                        </div>
+                                        <div className="card-body text-start p-0 my-2 my-md-4">
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <p className="card-title">
+                                                    婚禮／Wedding-1
+                                                </p>
 
-                <div className="card-group row row-cols-2 row-cols-md-2 row-cols-lg-4 my-1 mx-3 p-3 card-house">
-                    <div className="col">
-                        <div className="card-border " data-tilt>
-                            {/* <!-- data-tilt 特效 看是否保留 --> */}
-                            <div className="card-img ">
-                                <div className="ratios">
-                                    <img
-                                        className="img-fluid"
-                                        src={Img2}
-                                        alt=""
-                                    />
+                                                <i className="fas fa-heart"></i>
+                                            </div>
+                                            <div className="text-start">
+                                                NT 20
+                                                <div className="">
+                                                    <input
+                                                        className="form-check-input m-6"
+                                                        type="checkbox"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="card-body text-start p-0 my-2 my-md-4">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <p className="card-title fw-bold">
-                                        婚禮／Wedding-1
-                                    </p>
-
-                                    <input
-                                        className="form-check-input"
-                                        type="checkbox"
-                                        value=""
-                                        id="flexCheckDefault"
-                                    />
+                        </div> */}
+                        {display.map((v, i) => {
+                            const img = `${IMG_URL2}/${v.products_img}/a1.jpg`;
+                            console.log('img :>> ', img);
+                            return (
+                                <div
+                                    className="col-6 col-md-4 mt-2 px-3 mb-2"
+                                    key={v.id}
+                                >
+                                    <div className="card-border ">
+                                        <div className="card-img position-relative">
+                                            <div className="ratios">
+                                                <img
+                                                    className="img-fluid"
+                                                    src={img}
+                                                    alt=""
+                                                />
+                                            </div>
+                                            {v.status == 2 ? (
+                                                <span className="position-absolute top-0  translate-middle p-2  border-light rounded-circle download-badge">
+                                                    <span className="badge bg-secondary">
+                                                        已下載
+                                                    </span>
+                                                </span>
+                                            ) : (
+                                                ''
+                                            )}
+                                        </div>
+                                        <div className="card-body text-start p-0 my-2 my-md-4">
+                                            <label
+                                                htmlFor={v.id}
+                                                className="d-flex justify-content-between align-items-center"
+                                            >
+                                                <input
+                                                    className="form-check-input m-0"
+                                                    type="checkbox"
+                                                    value={v.id}
+                                                    id={v.id}
+                                                    checked={v.check}
+                                                    onChange={function (e) {
+                                                        handleCheck(e, i);
+                                                    }}
+                                                />
+                                                <p className="card-title fw-bold">
+                                                    {v.name}
+                                                </p>
+                                            </label>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="text-start">NT 20</div>
-                            </div>
+                            );
+                        })}
+                    </div>
+                    <Pagination
+                        total={counts}
+                        limit={8}
+                        offset={offset}
+                        setOffset={setOffset}
+                    />
+                    {/* pagination樣式 */}
+                    {/* <div className="container">
+                        <div className="pagination">
+                            <ul>
+                                <li>1</li>
+                                <li>2</li>
+                                <li>3</li>
+                                <li>4</li>
+                                <li>5</li>
+                            </ul>
                         </div>
-                    </div>
-                </div>
-                {/* <!-- Pagination --> */}
-                <div className="container">
-                    <div className="pagination">
-                        <ul>
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                            <li>4</li>
-                            <li>5</li>
-                        </ul>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/utils/context/FavoritesContext.js
+++ src/utils/context/FavoritesContext.js
@@ -4,7 +4,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // 共用
 import {
-    API_GET_WISHLIST,
+    API_GET_FAVORITES,
+    API_POST_FAVORITES,
     API_POST_AUTH_LOGIN,
     API_GET_PRODUCTS,
 } from '../config';
@@ -17,29 +18,64 @@ const FavoriteContext = React.createContext(
 );
 
 export function FavoritesProvider(props) {
-    //取得favorite 資料
-    const [favorites, setFavorites] = useState([]);
+    const auth = useAuthContext();
 
-    // 取得 Auth
-    const auth = useAuthContext([]);
-    //有登入axios存
-    if ((auth.isLogin = true)) {
-        let getfavorites = async () => {
-            try {
-                let response = await axios.post(API_GET_WISHLIST, {
-                    params: {
-                        userid: auth.user_id,
-                    },
-                });
-                console.log('res :>> ', response);
-                setFavorites(response.data);
-            } catch (err) {
-                console.log('err :>> ', err);
+    const [favorites, setFavorites] = useState([]);
+    const diff = useRef([]);
+    //抓 favorites
+    useEffect(() => {
+        if (auth.isLogin) {
+            let getfavorites = async () => {
+                try {
+                    let response = await axios.post(API_GET_FAVORITES, {
+                        params: {
+                            userid: auth.user.id,
+                        },
+                    });
+                    console.log('res :>> ', response);
+                    setFavorites(response.data);
+                } catch (err) {
+                    console.log('err :>> ', err);
+                }
+            };
+            getfavorites();
+            //沒登入存Loaclstorge
+        } else {
+            let f = localStorage.getItem('colorful-favorties');
+            if (!f) f = [];
+            setFavorites(f);
+        }
+    }, []);
+
+    // 寫入 Favorite
+    useEffect(
+        function () {
+            if (auth.isLogin) {
+                (async function () {
+                    try {
+                        const res = await axios.post(API_POST_FAVORITES, {
+                            params: {
+                                userID,
+                                diff,
+                            },
+                        });
+
+                        if (!res.result) {
+                            new Error('failed write favorites...');
+                        }
+                    } catch (err) {
+                        console.log('err :>> ', err);
+                    }
+                })();
+            } else {
+                localStorage.setItem('colorful-favorites', favorites);
             }
-        };
-        getfavorites;
-        //沒登入存Loaclstorge
-    } else console.log('hello');
+        },
+        [favorites]
+    );
+
+    // 事件處理器 (加入 刪除)
+
     // // DidMount
     // useEffect(function () {}, []);
     // // DidUpdate
@@ -50,72 +86,72 @@ export function FavoritesProvider(props) {
     // );
     // // event handler
 
-    class Localwishlist {
-        constructor(key) {
-            this._key = key;
-            this._value = JSON.parse(localStorage.getItem(key)); // 存wishlist的陣列
-            if (!this._value) this._value = [];
-            if (!Array.isArray(this._value))
-                console.log(
-                    __filename,
-                    ' :>> webstorage : value mest be a array'
-                );
-        }
+    //     class Localfavorites {
+    //         constructor(key) {
+    //             this._key = key;
+    //             this._value = JSON.parse(localStorage.getItem(key)); // 存wishlist的陣列
+    //             if (!this._value) this._value = [];
+    //             if (!Array.isArray(this._value))
+    //                 console.log(
+    //                     __filename,
+    //                     ' :>> webstorage : value mest be a array'
+    //                 );
+    //         }
 
-        // 取得資料（已從 string 轉成 JSON 物件）
-        goods() {
-            return this._value;
-        }
+    //         // 取得資料（已從 string 轉成 JSON 物件）
+    //         goods() {
+    //             return this._value;
+    //         }
 
-        // 放入一筆資料
-        add(goods) {
-            this._value.push(goods);
-            return this;
-        }
+    //         // 放入一筆資料
+    //         add(goods) {
+    //             this._value.push(goods);
+    //             return this;
+    //         }
 
-        // 放入多筆資料
-        apply(arr) {
-            const that = this;
-            arr.forEach(function (a) {
-                if (!that._value.some((b) => b.id === a.id)) {
-                    that._value.push(a);
-                }
-            });
-            return this;
-        }
+    //         // 放入多筆資料
+    //         apply(arr) {
+    //             const that = this;
+    //             arr.forEach(function (a) {
+    //                 if (!that._value.some((b) => b.id === a.id)) {
+    //                     that._value.push(a);
+    //                 }
+    //             });
+    //             return this;
+    //         }
 
-        // 刪除一筆資料
-        remove(product) {
-            this._value = this._value.filter(function (e) {
-                return e.id != product.id;
-            });
-            return this;
-        }
+    //         // 刪除一筆資料
+    //         remove(product) {
+    //             this._value = this._value.filter(function (e) {
+    //                 return e.id != product.id;
+    //             });
+    //             return this;
+    //         }
 
-        removeById(id) {
-            this._value = this._value.filter(function (e) {
-                return e.id != id;
-            });
-            return this;
-        }
+    //         removeById(id) {
+    //             this._value = this._value.filter(function (e) {
+    //                 return e.id != id;
+    //             });
+    //             return this;
+    //         }
 
-        // 檢視資料
-        log() {
-            console.log(`localStorage ${this._key}:>> `, this._value);
-        }
+    //         // 檢視資料
+    //         log() {
+    //             console.log(`localStorage ${this._key}:>> `, this._value);
+    //         }
 
-        // 儲存至 localStorage
-        save() {
-            localStorage.setItem(this._key, JSON.stringify(this._value));
-        }
+    //         // 儲存至 localStorage
+    //         save() {
+    //             localStorage.setItem(this._key, JSON.stringify(this._value));
+    //         }
 
-        // 清除所有
-        clear() {
-            localStorage.removeItem(this._key);
-        }
-    }
+    //         // 清除所有
+    //         clear() {
+    //             localStorage.removeItem(this._key);
+    //         }
+    //     }
+    // }
 }
-
 // Product
 export function FavoritesConsumer(props) {
     return <ProductsContext.id></ProductsContext.id>;
