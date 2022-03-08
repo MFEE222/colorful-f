287e25b46589894033f9c55a1bb939bee47163be
Po Yu Chen <pyshif@gmail.com>
2022-03-09 00:33:48 +0800

    把download 的版貼過來

--- src/components/Member/CollectContent.js
+++ src/components/Member/CollectContent.js
@@ -1,12 +1,67 @@
 import React from 'react';
 
-function CollectContent (props) {
+function CollectContent(props) {
+    const [counts, setCounts] = useState(); //資料總計
+    const [offset, setOffset] = useState(1); //分頁
+    const [display, setDisplay] = useState([]); //呈現
+
     return (
-        <div className="member-collect">
-            <div className="box">
-                <h1>Hello...0.0</h1>
+        <>
+            <div className="member-comment favorite">
+                {/* <!-- card --> */}
+                <div className="row  my-1 card-house">
+                    {display.map((v, i) => {
+                        const img = `${IMG_URL2}/${v.products_img}/a1.jpg`;
+                        // console.log('img :>> ', img);
+                        return (
+                            <div
+                                className="col-6 col-md-4 mt-2 px-3 mb-2"
+                                key={v.id}
+                            >
+                                <div className="card-border ">
+                                    <div className="card-img position-relative">
+                                        <div className="ratios">
+                                            <img
+                                                className="img-fluid"
+                                                src={img}
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="card-body text-start p-0 my-2 my-md-4">
+                                        <label
+                                            htmlFor={v.id}
+                                            className="d-flex justify-content-between align-items-center"
+                                        >
+                                            <input
+                                                className="form-check-input m-0"
+                                                type="checkbox"
+                                                value={v.id}
+                                                id={v.id}
+                                                checked={v.check}
+                                                onChange={function (e) {
+                                                    handleCheck(e, i);
+                                                }}
+                                            />
+                                            <p className="card-title fw-bold">
+                                                {v.name}
+                                            </p>
+                                        </label>
+                                        {/* </div> */}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <Pagination
+                    total={counts}
+                    limit={8}
+                    offset={offset}
+                    setOffset={setOffset}
+                />
             </div>
-        </div>
+        </>
     );
 }
 
