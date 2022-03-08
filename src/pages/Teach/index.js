d176566d676f7e3ff3c9b217d0c4f027222aae68
chiang-song-lang <afga3780@gmail.com>
2022-03-08 14:28:33 +0800

    teach

--- src/pages/Teach/index.js
+++ src/pages/Teach/index.js
@@ -1,75 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//
+import page1 from '../../images/教學頁/lr使用塗.jpg';
+import page2 from '../../images/teach-p2.jpg';
+import page4 from '../../images/教學頁/teach-p4.mp4';
+import video from '../../images/英文版手機教學.mpg';
+import Download from '../../images/教學頁/教學文件檔案.pdf';
 
 function Teach() {
     return (
-        <div className="container-fluid teach">
+        <div className="teach">
             {/* <!-- 第一頁 --> */}
-            <div className="row justify-content-center align-items-center page-one">
-                {/* <!-- 標題 --> */}
-                <div className="col-12 col-sm-4 p-3 page-one-left">
-                    <h1 className="text-center">COLORFUL</h1>
-                    <br />
-                    <p className="text-center">
-                        A Better Way to Organize Your Recipe
-                    </p>
-                </div>
-
-                {/* <!-- 右側圖 --> */}
-                <div className="col-12 col-sm-8 row page-one-right">
-                    {/* <!-- iphone - 1 --> */}
-                    <div className="col-12 col-sm-6 text-center iphone">
-                        <div className="iphone-screen">
-                            <img src="../../images/iphone-p1.png" alt="" />
-                        </div>
-                        {/* <!-- 螢幕內容全部用這個包 --> */}
-                        {/* <!-- <div className="screen-content">
-                          <div className="product">
-                              <img src="../../images/product-img.jpeg" alt=""/>
-                          </div>
-                      </div> --> */}
-                    </div>
-                    {/* <!-- iphone - 2 --> */}
-                    <div className="col-12 col-sm-6 text-center iphone">
-                        <div className="iphone-screen">
-                            <img src="../../images/iphone-p1.png" alt="" />
-                        </div>
-                        {/* <!-- 螢幕內容全部用這個包 --> */}
-                        <div className="screen-content">
-                            <img src="../../images/product-img.jpeg" alt="" />
-                        </div>
-                    </div>
-                </div>
+            <div className="align-items-center">
+                <img className="page-one" src={page1} alt="" />
             </div>
 
             {/* <!-- 第二頁 --> */}
             {/* <!-- flex container --> */}
-            <div className="row justify-content-center align-items-center page-two">
+            <div className="justify-content-center align-items-center page-two">
                 {/* <!-- flex item --> */}
-                <div className="col-12 col-lg-7 page-two-headline overflow-hidden">
-                    <h1 className="teach-font fs-1 fw-bold m-5 pe-4">
-                        How to Install Lightroom Presets for Desktop and Mobile?
-                    </h1>
-                    <p className="m-5 pe-5">
-                        Build your site in a few seconds based on ready-to-use
-                        blocks and sections, it's a lot of fun.
-                    </p>
-                    <div className="shape-ex3 m-5 overflow-hidden">
-                        <p>PDF download ↗</p>
+                <div className="row">
+                    <div className="col-12 col-lg-7 pt-5 page-two-headline overflow-hidden">
+                        <h1 className="teach-font fs-1 fw-bold m-5">
+                            How to Install Lightroom Presets for Desktop and
+                            Mobile?
+                        </h1>
+                        <p className="m-5 pe-5">
+                            Build your site in a few seconds based on
+                            ready-to-use blocks and sections, it's a lot of fun.
+                        </p>
+                        <div className="shape-ex3 m-5 overflow-hidden">
+                            <p>PDF download ↗</p>
+                        </div>
+                        {/* <Link to={Download} download>
+                            <button
+                                type="button"
+                                class="btn btn-primary btn-lg"
+                            >
+                                Download
+                            </button>
+                        </Link> */}
                     </div>
-                </div>
 
-                {/* <!-- flex item --> */}
-                <div className="col-12 col-lg-5 p-0">
-                    <img
-                        className="test w-100"
-                        src="./../../images/iphone-p2.jpg"
-                        alt=""
-                    />
+                    {/* <!-- flex item --> */}
+                    <div className="col-12 col-lg-5 p-0">
+                        <img className="test w-100" src={page2} alt="" />
+                    </div>
                 </div>
             </div>
 
             {/* <!-- 第三頁 --> */}
-            <div className="row justify-content-center align-items-center page-three">
+            <div className="justify-content-center align-items-center page-three">
                 <div className="col-12 ps-5 pt-5">
                     <h1 className="video-teach">
                         <i className="fas fa-play pe-3"></i>Video Teaching
@@ -81,6 +63,19 @@ function Teach() {
                     <div className="col-12 col-lg-6 vd-box">
                         <div className="box bg-light px-3 py-3 m-5">
                             <div className="ratios">
+                                {/* <video
+                                    className="w-100 video"
+                                    autoPlay
+                                    muted
+                                    loop
+                                    preload="true"
+                                >
+                                    <source
+                                        src={video}
+                                        alt="food-banner"
+                                        type="video/mp4"
+                                    />
+                                </video> */}
                                 <iframe
                                     className="video"
                                     src="https://www.youtube.com/embed/-EKH1f1DdC8"
@@ -88,9 +83,7 @@ function Teach() {
                                     allowfullscreen
                                 ></iframe>
                             </div>
-                            <div
-                                className="overflow-hidden t-v-text"
-                            >
+                            <div className="overflow-hidden t-v-text">
                                 <h2 className="my-3 fw-bold">
                                     Photographer's retouching process
                                 </h2>
@@ -116,9 +109,7 @@ function Teach() {
                                     allowfullscreen
                                 ></iframe>
                             </div>
-                            <div
-                                className="overflow-hidden t-v-text"
-                            >
+                            <div className="overflow-hidden t-v-text">
                                 <h2 className="my-3 fw-bold">
                                     Photographer's retouching process
                                 </h2>
@@ -136,8 +127,21 @@ function Teach() {
                 </div>
 
                 {/* <!-- 第四頁 --> */}
-                <div className="page-four px-0">
-                    <img class="image-4" src="./../../images/R.jpg" alt="" />
+                <div className="page-four">
+                    <video
+                        className="w-100 video"
+                        autoPlay
+                        muted
+                        loop
+                        preload="true"
+                    >
+                        <source
+                            src={page4}
+                            alt="food-banner"
+                            type="video/mp4"
+                        />
+                    </video>
+                    {/* <img class="image-4" src={} alt="" /> */}
                 </div>
             </div>
 
