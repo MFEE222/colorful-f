e306216293517d88b3b57e73aec80b9164d28dca
Queena Tao <queena113@gmail.com>
2022-03-09 08:20:35 +0800

    合併 landing

--- src/pages/Home/landing.js
+++ src/pages/Home/landing.js
@@ -5,32 +5,32 @@ import 'animate.css';
 function Landing(props) {
     return (
         <div className="landing animate__animated animate__bounce animate__faster">
-            <div class="container animated bounceInDown">
-                <div class="container__item landing-page-container">
-                    <div class="content__wrapper">
-                        <p class="coords">
+            <div className="container animated bounceInDown">
+                <div className="container__item landing-page-container">
+                    <div className="content__wrapper">
+                        <p className="coords">
                             N 24° 25' 6.812" / W 121° 13' 19.59"
                         </p>
 
-                        <div class="ellipses-container">
-                            <h2 class="greeting">Hello</h2>
+                        <div className="ellipses-container">
+                            <h2 className="greeting">Hello</h2>
 
-                            <div class="ellipses ellipses__outer--thin">
-                                <div class="ellipses ellipses__orbit"></div>
+                            <div className="ellipses ellipses__outer--thin">
+                                <div className="ellipses ellipses__orbit"></div>
                             </div>
 
-                            <div class="ellipses ellipses__outer--thick"></div>
+                            <div className="ellipses ellipses__outer--thick"></div>
                         </div>
 
-                        <div class="scroller">
+                        <div className="scroller">
                             <a href="#page1">
-                                <p class="page-title">home</p>
+                                <p className="page-title">home</p>
                             </a>
 
-                            <div class="timeline">
-                                <span class="timeline__unit"></span>
-                                <span class="timeline__unit timeline__unit--active"></span>
-                                <span class="timeline__unit"></span>
+                            <div className="timeline">
+                                <span className="timeline__unit"></span>
+                                <span className="timeline__unit timeline__unit--active"></span>
+                                <span className="timeline__unit"></span>
                             </div>
                         </div>
                     </div>
