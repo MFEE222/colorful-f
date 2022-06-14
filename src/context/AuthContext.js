e71e33c24b271dfb02a8e85f7b1678966ff81d27
Po Yu Chen <pyshif@gmail.com>
2022-06-14 09:45:44 +0800

    add jest intellisense library and complet AuthConext reqeust API test

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import React, { useReducer, useState, useEffect } from 'react';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
 // FIXME: remove
 import {
     API_POST_AUTH_LOGIN,
@@ -31,52 +33,44 @@ import { routes } from '../utils/routes';
 // Context
 const AuthContext = React.createContext('wrap not correct');
 
-// TODO: /api/auth/
-// TODO: /api/auth/token
-// TODO: /api/auth/signin
-// TODO: /api/auth/signup
-// TODO: /api/auth/forgot
-
 // Provider
-// export function AuthProvider(props) {
-//     // state
-//     function reducer(state, action) {
-//         switch (action) {
-//             default:
-//                 throw new Error();
-//         }
-//     }
+export function AuthProvider(props) {
+    // state
+    function reducer(state, action) {
+        switch (action) {
+            default:
+                throw new Error();
+        }
+    }
 
-//     const [state, dispatch] = useReducer(reducer, {
-//         accessToken: '',
-//         userEmail: '',
-//         userName: '',
-//         userBirthday: '',
-//         userGender: '',
-//         isSignin: false,
-//     });
+    const [state, dispatch] = useReducer(reducer, {
+        accessToken: '',
+        userEmail: '',
+        userName: '',
+        isSignin: false,
+    });
 
-//     const [isLogin, setIsLogin] = useState(false);
-//     const [user, setUser] = useState({ id: 1 });
-//     const [allowReset, setAllowReset] = useState(false);
-//     const [showLoginModal, setShowLoginModal] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [user, setUser] = useState({ id: 1 });
+    const [allowReset, setAllowReset] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
 
-//     // share
-//     const share = {
-//         state,
-//         dispatch,
-//     };
+    // share
+    const share = {
+        state,
+        dispatch,
+    };
 
-//     // render
-//     return (
-//         <AuthContext.Provider value={shared}>
-//             {/* {props.children} */}
-//             <LoginModal shared={shared} onHide={() => setShowLoginModal(false)}>
-//                 {props.children}
-//             </LoginModal>
-//         </AuthContext.Provider>
-//     );
-// }
+    // render
+    return (
+        <AuthContext.Provider value={shared}>
+            {/* {props.children} */}
+            <LoginModal shared={shared} onHide={() => setShowLoginModal(false)}>
+                {props.children}
+            </LoginModal>
+        </AuthContext.Provider>
+    );
+}
 
 // Consumer (old version)
 // export function AuthConsumer(props) {
@@ -89,134 +83,134 @@ export function useAuthContext() {
 }
 
 // 內部元件
-function LoginModal(props) {
-    const { showLoginModal, setShowLoginModal } = props.shared;
-    // console.log('props.shared :>> ', props.shared);
-    const [user, setUser] = useState({ account: '', password: '' });
+// function LoginModal(props) {
+//     const { showLoginModal, setShowLoginModal } = props.shared;
+//     // console.log('props.shared :>> ', props.shared);
+//     const [user, setUser] = useState({ account: '', password: '' });
 
-    function handleChange(e) {
-        const newUser = { ...user };
-        newUser[e.target.name] = e.target.value;
-        setUser(newUser);
-    }
+//     function handleChange(e) {
+//         const newUser = { ...user };
+//         newUser[e.target.name] = e.target.value;
+//         setUser(newUser);
+//     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.shared.login(user);
-    }
+//     function handleSubmit(e) {
+//         e.preventDefault();
+//         props.shared.login(user);
+//     }
 
-    const temp = (
-        <div>
-            <div className="d-flex align-content-center mt-2">
-                <label htmlFor="email" className="col-3 labal">
-                    帳號
-                </label>
-                <input
-                    className="col mb-2"
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="d-flex align-content-center mt-2">
-                <label htmlFor="password" className="col-3 labal">
-                    密碼
-                </label>
-                <input
-                    className="col mb-2"
-                    type="text"
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                />
-            </div>
-            {/* <button type="submit" onClick={handleSubmit} className="row">
-                send
-            </button> */}
-        </div>
-    );
-    return showLoginModal ? (
-        <>
-            <div
-                className="auth-modal"
-                onClick={function (e) {
-                    setShowLoginModal(false);
-                }}
-            >
-                <div
-                    className="box row"
-                    onClick={function (e) {
-                        e.stopPropagation();
-                    }}
-                >
-                    <div className="col-12 col-md-9 modal-left">
-                        <div className="header d-flex my-3 align-content-center justify-content-center">
-                            <div className="text-center">您有會員嗎？</div>
-                        </div>
-                        <div className="border-top w-100 my-3"></div>
-                        <div className="row d-flex justify-content-center  gx-5 align-items-stretch mt-5">
-                            <div className="col-12">{temp}</div>
-                            <button
-                                type="submit"
-                                className="col-auto justify-content-end 
-                                    align-items-end login"
-                                // type="submit"
-                                onClick={handleSubmit}
-                            >
-                                <p className="m-0">登入</p>
-                            </button>
-                            <div className="col-12">
-                                <div className="d-flex justify-content-center align-items-center">
-                                    <Link
-                                        className="signup mt-2"
-                                        onClick={function (e) {
-                                            setShowLoginModal(false);
-                                        }}
-                                        to={routes.signup}
-                                    >
-                                        前往註冊
-                                        <i className="ps-2 fas fa-long-arrow-alt-right"></i>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="row justify-content-center mt-2">
-                                擁有會員能享有更多專屬功能
-                            </div>
-                        </div>
-                    </div>
-                    <div className="d-none d-md-block col-md-3 modal-right justify-content-end">
-                        <img className="right-pic" src={login} alt="" />
-                    </div>
-                </div>
-            </div>
-            {props.children}
-        </>
-    ) : (
-        // <div>
-        //     {/* 你這個要用 position: absolute */}
-        //     <div
-        //         style={{
-        //             backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        //             width: '300px',
-        //             height: '300px',
-        //         }}
-        //         className="position-absoulte top-0 left-0 right-0 bottom-0"
-        //     >
-        //         <h1>請登入</h1>
-        //         <h2
-        //             onClick={function () {
-        //                 setShowLoginModal(false);
-        //             }}
-        //         >
-        //             取消
-        //         </h2>
-        //     </div>
-        //     {props.children}
-        // </div>
-        <>{props.children}</>
-    );
-}
+//     const temp = (
+//         <div>
+//             <div className="d-flex align-content-center mt-2">
+//                 <label htmlFor="email" className="col-3 labal">
+//                     帳號
+//                 </label>
+//                 <input
+//                     className="col mb-2"
+//                     type="email"
+//                     name="email"
+//                     value={user.email}
+//                     onChange={handleChange}
+//                 />
+//             </div>
+//             <div className="d-flex align-content-center mt-2">
+//                 <label htmlFor="password" className="col-3 labal">
+//                     密碼
+//                 </label>
+//                 <input
+//                     className="col mb-2"
+//                     type="text"
+//                     name="password"
+//                     value={user.password}
+//                     onChange={handleChange}
+//                 />
+//             </div>
+//             {/* <button type="submit" onClick={handleSubmit} className="row">
+//                 send
+//             </button> */}
+//         </div>
+//     );
+//     return showLoginModal ? (
+//         <>
+//             <div
+//                 className="auth-modal"
+//                 onClick={function (e) {
+//                     setShowLoginModal(false);
+//                 }}
+//             >
+//                 <div
+//                     className="box row"
+//                     onClick={function (e) {
+//                         e.stopPropagation();
+//                     }}
+//                 >
+//                     <div className="col-12 col-md-9 modal-left">
+//                         <div className="header d-flex my-3 align-content-center justify-content-center">
+//                             <div className="text-center">您有會員嗎？</div>
+//                         </div>
+//                         <div className="border-top w-100 my-3"></div>
+//                         <div className="row d-flex justify-content-center  gx-5 align-items-stretch mt-5">
+//                             <div className="col-12">{temp}</div>
+//                             <button
+//                                 type="submit"
+//                                 className="col-auto justify-content-end
+//                                     align-items-end login"
+//                                 // type="submit"
+//                                 onClick={handleSubmit}
+//                             >
+//                                 <p className="m-0">登入</p>
+//                             </button>
+//                             <div className="col-12">
+//                                 <div className="d-flex justify-content-center align-items-center">
+//                                     <Link
+//                                         className="signup mt-2"
+//                                         onClick={function (e) {
+//                                             setShowLoginModal(false);
+//                                         }}
+//                                         to={routes.signup}
+//                                     >
+//                                         前往註冊
+//                                         <i className="ps-2 fas fa-long-arrow-alt-right"></i>
+//                                     </Link>
+//                                 </div>
+//                             </div>
+//                             <div className="row justify-content-center mt-2">
+//                                 擁有會員能享有更多專屬功能
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="d-none d-md-block col-md-3 modal-right justify-content-end">
+//                         <img className="right-pic" src={login} alt="" />
+//                     </div>
+//                 </div>
+//             </div>
+//             {props.children}
+//         </>
+//     ) : (
+//         // <div>
+//         //     {/* 你這個要用 position: absolute */}
+//         //     <div
+//         //         style={{
+//         //             backgroundColor: 'rgba(0, 0, 0, 0.2)',
+//         //             width: '300px',
+//         //             height: '300px',
+//         //         }}
+//         //         className="position-absoulte top-0 left-0 right-0 bottom-0"
+//         //     >
+//         //         <h1>請登入</h1>
+//         //         <h2
+//         //             onClick={function () {
+//         //                 setShowLoginModal(false);
+//         //             }}
+//         //         >
+//         //             取消
+//         //         </h2>
+//         //     </div>
+//         //     {props.children}
+//         // </div>
+//         <>{props.children}</>
+//     );
+// }
 
 // API to backend
 export async function requestAuth(access_token) {
@@ -227,7 +221,7 @@ export async function requestAuth(access_token) {
             headers: { Authorization: 'Bearer ' + access_token },
         });
 
-        console.log('response :>> ', response);
+        // console.log('response :>> ', response);
 
         if (response.status != 200) {
             throw new Error();
@@ -256,6 +250,9 @@ export async function requestSignIn(account, password) {
         if (response.status != 200 || !access_token) {
             throw new Error();
         }
+
+        // const user = jwt_decode(access_token);
+        // console.log('user :>> ', user);
 
         return access_token;
     } catch (err) {
@@ -302,7 +299,7 @@ export async function requestSignUp(
             },
         });
 
-        console.log('response :>> ', response);
+        // console.log('response :>> ', response);
         if (response.status != 200) {
             throw new Error();
         }
@@ -367,7 +364,7 @@ export async function requestHealthCheck() {
             url: GET_AUTH_HEALTH,
         });
 
-        console.log('response :>> ', response);
+        // console.log('response :>> ', response);
 
         if (response.status != 200) {
             throw new Error();
--- src/context/__tests__/AuthContext.js
+++ src/context/__tests__/AuthContext.js
@@ -8,10 +8,64 @@ import {
     requestHealthCheck,
 } from '../AuthContext';
 
-// it('post sign in request to backend', () => {});
+const name = '';
+const email = ''; // email also is account
+const password = '';
+const hint = '';
 
 it('auth health check', async () => {
     const result = await requestHealthCheck();
 
     expect(result).toBeTruthy();
 });
+
+// once register successful, will receive authentication email.
+// and repeat register will lead to HTTP 403.
+
+// it('sign up test', async () => {
+//     const result = await requestSignUp(
+//         name,
+//         email,
+//         password,
+//         password,
+//         hint
+//     );
+
+//     expect(result).toBeTruthy();
+// });
+
+let access_token;
+
+it('sign in test', async () => {
+    const result = await requestSignIn(email, password);
+
+    access_token = result;
+
+    expect(result).not.toBeNull();
+});
+
+it('auth (access_token) test', async () => {
+    const result = await requestAuth(access_token);
+
+    expect(result).toBeTruthy();
+});
+
+// FIXME: make cookie in testing environment (because refresh_token store in cookies)
+// it('sign out test', async () => {
+//     const result = await requestSignOut();
+
+//     expect(result).toBeTruthy();
+// });
+
+// it('access token refresh test', async () => {
+//     const result = await requsetAccessToken();
+
+//     expect(result).not.toBeNull();
+// });
+
+// !!! each success response has send reset password email for you.
+// it('forgot password test', async () => {
+//     const result = await requestForgotPassword(email, hint);
+
+//     expect(result).toBeTruthy();
+// });
