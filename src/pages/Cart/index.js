60c483b8474f3e063a73f07290d462cc161c6c36
Po Yu Chen <pyshif@gmail.com>
2022-08-27 02:50:48 +0800

    create, update | (Cart.js, Checkout.js), (Cart/index.js) | routes.ts

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { routes } from '../../utils/routes';
+import Routes from '../../utils/ts-routes';
 
 import CartContent from './CartContent';
 import CartPayment from './CartPayment';
+import Checkout from './Checkout';
+import Cart from './Cart';
 
 function Cart(props) {
     return (
         <>
             <Switch>
                 {/* 結帳付款頁 */}
-                <Route path={routes.checkout}>
-                    <CartPayment />
+                <Route path={Routes.CART.CHECKOUT}>
+                    <Checkout />
                 </Route>
 
                 {/* 購物車主頁 */}
-                <Route path={routes.cart}>
-                    <CartContent />
+                <Route path={Routes.CART.INDEX}>
+                    <Cart />
                 </Route>
             </Switch>
         </>
