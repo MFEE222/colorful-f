f3a23a63a9be501747f09d9dbf619f574f7236bd
Po Yu Chen <pyshif@gmail.com>
2022-10-01 03:12:13 +0800

    update webpack.sandbox.js

--- webpack.sandbox.js
+++ webpack.sandbox.js
@@ -1,10 +1,31 @@
 const { merge } = require('webpack-merge');
 const dev = require('./webpack.dev');
+// const TerserPlugin = require('terser-webpack-plugin');
+// const BundleAnalyzerPlugin =
+//     require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env) => {
     return merge(dev(env), {
         entry: {
             main: './src/sandbox.js',
         },
+
+        // plugins: [new BundleAnalyzerPlugin()],
+
+        // module: {
+        //     rules: [
+        //         {
+        //             sideEffects: false,
+        //         },
+        //     ],
+        // },
+
+        // optimization: {
+        //     usedExports: true,
+        //     minimize: true,
+        //     minimizer: [new TerserPlugin()],
+        // },
+
+        // devtool: 'source-map',
     });
 };
